07d46b10f52c1dbfecbea7009d56e8414f0bc573
Shruthi42 <13177030+Shruthi42@users.noreply.github.com>
2020-09-04 21:45:09 +0530

    Flake8

--- Tests/ML/utils/test_image_util.py
+++ Tests/ML/utils/test_image_util.py
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-from typing import Any, List, Optional, Tuple
+from typing import Any, List, Optional
 
 import numpy as np
 import pytest
