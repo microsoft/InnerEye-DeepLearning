9492a0bb4bdf6dbba63394dc0cf66cd826156022
Anton Schwaighofer <antonsc@microsoft.com>
2020-09-24 20:16:02 +0100

    running

--- InnerEye/start_mlflow.py
+++ InnerEye/start_mlflow.py
@@ -7,20 +7,20 @@ import os
 import sys
 from pathlib import Path
 
-from azureml.core import Workspace
+import param
 from mlflow.server import _run_server
 from mlflow.server.handlers import initialize_backend_stores
 
 from InnerEye.Azure.azure_config import AzureConfig
 from InnerEye.Common import fixed_paths
 from InnerEye.Common.common_util import print_exception
+from InnerEye.Common.generic_parsing import GenericConfig
 
 
-def get_workspace(settings_file: Path) -> Workspace:
-    azure_config = AzureConfig.from_yaml(settings_file)
-    ws = azure_config.get_workspace()
-    print(ws.get_details())
-    return ws
+class MLFlowSettings(GenericConfig):
+    settings: Path = param.ClassSelector(class_=Path, default=fixed_paths.SETTINGS_YAML_FILE,
+                                         doc="File containing subscription details, typically your settings.yml")
+    port: int = param.Integer(default=8008, doc="The port on which the MLFlow server will operate.")
 
 
 def aml_ui(backend_store_uri, default_artifact_root, port, host):
@@ -36,14 +36,16 @@ def aml_ui(backend_store_uri, default_artifact_root, port, host):
         sys.exit(1)
 
 
-def start_ui(settings_file: Path):
-    ws = get_workspace(settings_file)
+def start_ui(project_root: Path) -> None:
+    server_settings = MLFlowSettings.parse_args()
+    azure_config = AzureConfig.from_yaml(server_settings.settings, project_root=project_root)
+    ws = azure_config.get_workspace()
     uri = ws.get_mlflow_tracking_uri()
     # Workaround for a bug in MLFlow: Without this, no artifacts will be displayed.
     os.environ["MLFLOW_TRACKING_URI"] = uri
-    # Host argument needs to be set to 0.0.0.0 to have the server access from outside
-    aml_ui(backend_store_uri=uri, default_artifact_root=uri, port=80, host="0.0.0.0")
+    # Host argument needs to be set to 0.0.0.0 to have the server accessible from outside
+    aml_ui(backend_store_uri=uri, default_artifact_root=uri, port=server_settings.port, host="0.0.0.0")
 
 
 if __name__ == "__main__":
-    start_ui(settings_file=fixed_paths.SETTINGS_YAML_FILE)
+    start_ui(project_root=fixed_paths.repository_root_directory())
