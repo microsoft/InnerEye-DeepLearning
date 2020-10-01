6ff29774cfa6f19a69fe988d611d01b30f0cf35b
jananava <jananava@microsoft.com>
2020-10-01 15:55:14 +0100

    update

--- InnerEye/ML/reports/segmentation_report.py
+++ InnerEye/ML/reports/segmentation_report.py
@@ -9,10 +9,11 @@ from IPython.display import display
 from pandas import DataFrame
 
 from InnerEye.ML.reports.notebook_report import print_header
+from InnerEye.ML.utils.csv_util import OutlierType, extract_outliers
 from InnerEye.ML.utils.metrics_constants import MetricsFileColumns
 
 
-def plot_scores_for_csv(path_csv: str) -> None:
+def plot_scores_for_csv(path_csv: str, outlier_range: float) -> None:
     """
     Displays all the tables and figures given a csv file with segmentation metrics
     Columns expected: Patient,Structure,Dice,HausdorffDistance_mm,MeanDistance_mm
@@ -20,35 +21,37 @@ def plot_scores_for_csv(path_csv: str) -> None:
     print(f"Reading raw metrics data from: {path_csv}")
     df = pd.read_csv(path_csv)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 150):
-        display_metric(df, MetricsFileColumns.Dice.value, high_values_are_good=True)
-        display_metric(df, MetricsFileColumns.HausdorffDistanceMM.value, high_values_are_good=False)
-        display_metric(df, MetricsFileColumns.MeanDistanceMM.value, high_values_are_good=False)
+        display_metric(df, MetricsFileColumns.Dice.value, outlier_range, high_values_are_good=True)
+        display_metric(df, MetricsFileColumns.HausdorffDistanceMM.value, outlier_range, high_values_are_good=False)
+        display_metric(df, MetricsFileColumns.MeanDistanceMM.value, outlier_range, high_values_are_good=False)
 
 
-def display_metric(df: pd.DataFrame, metric_name: str, high_values_are_good: bool) -> None:
+def display_metric(df: pd.DataFrame, metric_name: str, outlier_range: float, high_values_are_good: bool) -> None:
     """
     Displays a dataframe with a metric per structure, first showing the
     :param df: The dataframe with metrics per structure
     :param metric_name: The metric to sort by.
+    :param outlier_range: The standard deviation range data points must fall outside of to be considered an outlier
     :param high_values_are_good: If true, high values of the metric indicate good performance. If false, low
     values indicate good performance.
     """
     print_header(metric_name, level=2)
     # Display with best structures first.
     display(describe_score(df, metric_name, ascending=(not high_values_are_good)))
-    print_header(f"Worst patients by {metric_name}", level=3)
-    display(worst_patients(df, metric_name, ascending=high_values_are_good))
+    print_header(f"Worst patients by {metric_name} (< {outlier_range} sd)", level=3)
+    display(display_outliers(df, outlier_range, metric_name, high_values_are_good))
     boxplot_per_structure(df, column_name=metric_name, title=metric_name)
     plt.show()
 
 
-def worst_patients(df: pd.DataFrame, metric_name: str, ascending: bool) -> str:
+def display_outliers(df: pd.DataFrame, outlier_range: float,
+                     metric_name: str, high_values_are_good: bool) -> pd.DataFrame:
     """
     Prints a dataframe that contains the 20 worst patients by the given metric. "Worst" is determined by
     being at the start of the list when sorting by the metric either ascending or descending.
     """
-    df2 = df.sort_values(by=metric_name, ascending=ascending).head(20)
-    return df2
+    return extract_outliers(df, outlier_range, metric_name,
+                            OutlierType.LOW if high_values_are_good else OutlierType.HIGH)
 
 
 def describe_score(df: pd.DataFrame, metric_name: str, ascending: bool = True) -> pd.DataFrame:
