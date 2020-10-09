8e092c4f73e47a8822ad91240edb21993e773cdf
melanibe <mebernha@microsoft.com>
2020-10-09 16:37:29 +0200

    add typing

--- Tests/ML/utils/test_dataset_utils.py
+++ Tests/ML/utils/test_dataset_utils.py
@@ -2,13 +2,13 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-from io import StringIO
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 from InnerEye.ML.utils.dataset_util import CategoricalToOneHotEncoder
 
 
-def test_one_hot_encoder_with_infinite_values():
+def test_one_hot_encoder_with_infinite_values() -> None:
     df = pd.DataFrame(columns=["categorical"])
     df["categorical"] = ["F", "M", np.inf]
     encoder = CategoricalToOneHotEncoder.create_from_dataframe(df, ["categorical"])
