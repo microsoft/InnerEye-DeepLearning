e6f7744f6811d7df7daf6e0d15d57567dfa97a49
Melissa Bristow <mebristo@microsoft.com>
2020-10-13 19:50:39 +0000

    remove config

--- InnerEye/ML/configs/segmentation/ProstatePaper.py
+++
@@ -1,9 +0,0 @@
-from InnerEye.ML.configs.segmentation.ProstateBase import ProstateBase
-
-
-class ProstatePaper(ProstateBase):
-    def __init__(self) -> None:
-        super().__init__(
-            azure_dataset_id="89e385d5-a511-426c-9675-aa9f9e7e5a85_no_geonorm",
-            save_start_epoch=20,
-            disable_extra_postprocessing=True)
