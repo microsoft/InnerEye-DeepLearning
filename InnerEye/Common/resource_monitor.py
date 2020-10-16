bd0ddbb8e0e31c993e45f0eede563420e4913d48
jananava <jananava@microsoft.com>
2020-10-16 17:23:48 +0100

    update

--- InnerEye/Common/resource_monitor.py
+++ InnerEye/Common/resource_monitor.py
@@ -57,9 +57,9 @@ class ResourceMonitor(Process):
                 if len(gpus) > 0:
                     for gpu in gpus:
                         log_to_azure_and_tb('Diagnostics/GPU_{}_CUDA_Memory_Reserved'.format(gpu.id),
-                                            torch.cuda.memory_reserved(gpu.id))
+                                            torch.cuda.memory_reserved(int(gpu.id)))
                         log_to_azure_and_tb('Diagnostics/GPU_{}_CUDA_Memory_Allocated'.format(gpu.id),
-                                            torch.cuda.memory_allocated(gpu.id))
+                                            torch.cuda.memory_allocated(int(gpu.id)))
                         log_to_azure_and_tb('Diagnostics/GPU_{}_Load_Percent'.format(gpu.id),
                                             gpu.load * 100)
                         log_to_azure_and_tb('Diagnostics/GPU_{}_MemUtil_Percent'.format(gpu.id),
