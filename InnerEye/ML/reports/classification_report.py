0a2c1143cc7ea400dc24aa6b0788976d3c5090cb
Shruthi42 <13177030+Shruthi42@users.noreply.github.com>
2020-10-21 22:46:26 +0530

    Update

--- InnerEye/ML/reports/classification_report.py
+++ InnerEye/ML/reports/classification_report.py
@@ -5,13 +5,18 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+import numpy as np
 
 from dataclasses import dataclass
 from sklearn.metrics import precision_recall_curve, roc_curve, auc, roc_auc_score
+from enum import Enum
+from pathlib import Path
 
 from InnerEye.ML.reports.notebook_report import print_header
 from InnerEye.Common.metrics_dict import MetricsDict
 from InnerEye.ML.utils.metrics_constants import LoggingColumns
+from InnerEye.ML.pipelines.scalar_inference import ScalarInferencePipelineBase
 
 
 @dataclass
@@ -22,10 +27,24 @@ class Results:
     false_negatives: pd.DataFrame
 
 
-def plot_auc(x_values, y_values, title, ax):
+class ReportedMetrics(Enum):
+    OptimalThreshold = "optimal_threshold"
+    AUC_PR = "auc_pr"
+    AUC_ROC = "auc_roc"
+
+
+def get_results(csv: Path) -> ScalarInferencePipelineBase.Result:
+    df = pd.read_csv(csv)
+    labels = df[LoggingColumns.Label.value]
+    model_outputs = df[LoggingColumns.ModelOutput.value]
+    subjects = df[LoggingColumns.Patient.value]
+    return ScalarInferencePipelineBase.Result(subject_ids=subjects, labels=labels, model_outputs=model_outputs)
+
+
+def plot_auc(x_values: np.ndarray, y_values: np.ndarray, title: str, ax: Axes):
     ax.plot(x_values, y_values)
-    ax.set_xlim((0, 1))
-    ax.set_ylim((0, 1))
+    ax.set_xlim(left=0, right=1)
+    ax.set_ylim(bottom=0, top=1)
     ax.set_title(title)
 
     # write values of points
@@ -33,47 +52,67 @@ def plot_auc(x_values, y_values, title, ax):
         ax.annotate(f"{x:0.3f}, {y:0.3f}", xy = (x, y), xytext=(15, 0), textcoords='offset points')
 
 
-def plot_pr_and_roc_curves_from_csv(metrics_csv):
+def plot_pr_and_roc_curves_from_csv(metrics_csv: Path):
     print_header("ROC and PR curves", level=3)
-    df = pd.read_csv(metrics_csv)
+    results = get_results(metrics_csv)
 
     _, ax = plt.subplots(1, 2)
 
-    fpr, tpr, thresholds = roc_curve(df[LoggingColumns.Label.value], df[LoggingColumns.ModelOutput.value])
+    fpr, tpr, thresholds = roc_curve(results.labels, results.model_outputs)
     plot_auc(fpr, tpr, "ROC Curve", ax[0])
-    precision, recall, thresholds = precision_recall_curve(df[LoggingColumns.Label.value],
-                                                           df[LoggingColumns.ModelOutput.value])
+    precision, recall, thresholds = precision_recall_curve(results.labels, results.model_outputs)
     plot_auc(precision, recall, "PR Curve", ax[1])
 
     plt.show()
 
 
-def print_metrics(val_metrics_csv, test_metrics_csv):
-    print_header("Test metrics", level=3)
-    df_val = pd.read_csv(val_metrics_csv)
-    fpr, tpr, thresholds = roc_curve(df_val[LoggingColumns.Label.value], df_val[LoggingColumns.ModelOutput.value])
+def get_metric(val_metrics_csv: Path, test_metrics_csv: Path, metric: ReportedMetrics):
+    results_val = get_results(val_metrics_csv)
+    fpr, tpr, thresholds = roc_curve(results_val.labels, results_val.model_outputs)
     optimal_idx = MetricsDict.get_optimal_idx(fpr=fpr, tpr=tpr)
     optimal_threshold = thresholds[optimal_idx]
+
+    if metric is ReportedMetrics.OptimalThreshold:
+        return optimal_threshold
+
+    results_test = get_results(test_metrics_csv)
+
+    if metric is ReportedMetrics.AUC_ROC:
+        return roc_auc_score(results_test.labels, results_test.model_outputs)
+    elif metric is ReportedMetrics.AUC_PR:
+        precision, recall, _ = precision_recall_curve(results_test.labels, results_test.model_outputs)
+        return auc(recall, precision)
+    else:
+        raise ValueError("Unknown metric")
+
+
+def print_metrics(val_metrics_csv: Path, test_metrics_csv: Path):
+    print_header("Test metrics", level=3)
+    optimal_threshold = get_metric(val_metrics_csv=val_metrics_csv,
+                                   test_metrics_csv=test_metrics_csv,
+                                   metric=ReportedMetrics.OptimalThreshold)
 
     print(f"Optimal threshold: {optimal_threshold}")
 
-    df_test = pd.read_csv(test_metrics_csv)
-    roc_auc = roc_auc_score(df_test[LoggingColumns.Label.value], df_test[LoggingColumns.ModelOutput.value])
+    roc_auc = get_metric(val_metrics_csv=val_metrics_csv,
+                                   test_metrics_csv=test_metrics_csv,
+                                   metric=ReportedMetrics.AUC_ROC)
     print(f"AUC PR: {roc_auc}")
-    precision, recall, _ = precision_recall_curve(df_test[LoggingColumns.Label.value],
-                                                  df_test[LoggingColumns.ModelOutput.value])
-    pr_auc = auc(recall, precision)
+
+    pr_auc = get_metric(val_metrics_csv=val_metrics_csv,
+                                   test_metrics_csv=test_metrics_csv,
+                                   metric=ReportedMetrics.AUC_PR)
     print(f"AUC PR: {pr_auc}")
 
 
-def get_correct_and_misclassified_examples(val_metrics_csv, test_metrics_csv):
+def get_correct_and_misclassified_examples(val_metrics_csv: Path, test_metrics_csv: Path):
     df_val = pd.read_csv(val_metrics_csv)
     fpr, tpr, thresholds = roc_curve(df_val[LoggingColumns.Label.value], df_val[LoggingColumns.ModelOutput.value])
     optimal_idx = MetricsDict.get_optimal_idx(fpr=fpr, tpr=tpr)
     optimal_threshold = thresholds[optimal_idx]
 
     df_test = pd.read_csv(test_metrics_csv)
-    df_test["predicted"] = df_test.apply(lambda x: int(x[LoggingColumns.ModelOutput.value] > optimal_threshold), axis=1)
+    df_test["predicted"] = df_test.apply(lambda x: int(x[LoggingColumns.ModelOutput.value] >= optimal_threshold), axis=1)
 
     true_positives = df_test[(df_test["predicted"] == 1) & (df_test[LoggingColumns.Label.value] == 1)]
     false_positives = df_test[(df_test["predicted"] == 1) & (df_test[LoggingColumns.Label.value] == 0)]
@@ -86,24 +125,36 @@ def get_correct_and_misclassified_examples(val_metrics_csv, test_metrics_csv):
                    false_negatives=false_negatives)
 
 
-def print_top_best_and_worst(val_metrics_csv, test_metrics_csv, k: int):
+def get_k_best_and_worst_performing(val_metrics_csv: Path, test_metrics_csv: Path, k: int):
     results = get_correct_and_misclassified_examples(val_metrics_csv=val_metrics_csv,
                                                      test_metrics_csv=test_metrics_csv)
 
     # sort by model_output
-    results.true_positives = results.true_positives.sort_values(by=LoggingColumns.Label.value, ascending=False)
-    results.true_negatives = results.true_negatives.sort_values(by=LoggingColumns.Label.value, ascending=True)
-    results.false_positives = results.true_positives.sort_values(by=LoggingColumns.Label.value, ascending=False)
-    results.false_negatives = results.true_negatives.sort_values(by=LoggingColumns.Label.value, ascending=True)
+    sorted = Results(true_positives=
+                     results.true_positives.sort_values(by=LoggingColumns.ModelOutput.value, ascending=False).head(k),
+                     true_negatives=
+                     results.true_negatives.sort_values(by=LoggingColumns.ModelOutput.value, ascending=True).head(k),
+                     false_positives=
+                     results.false_positives.sort_values(by=LoggingColumns.ModelOutput.value, ascending=False).head(k),
+                     false_negatives=
+                     results.false_negatives.sort_values(by=LoggingColumns.ModelOutput.value, ascending=True).head(k))
+    return sorted
+
+
+def print_k_best_and_worst_performing(val_metrics_csv: Path, test_metrics_csv: Path, k: int):
+
+    results = get_k_best_and_worst_performing(val_metrics_csv=val_metrics_csv,
+                                              test_metrics_csv=test_metrics_csv,
+                                              k=k)
 
     print_header(f"Top {k} false positives")
-    print(results.false_positives[LoggingColumns.Patient.value].head(k))
+    print(results.false_positives[LoggingColumns.Patient.value])
 
     print_header(f"Top {k} false negatives")
-    print(results.false_negatives[LoggingColumns.Patient.value].head(k))
+    print(results.false_negatives[LoggingColumns.Patient.value])
 
     print_header(f"Top {k} true positives")
-    print(results.true_positives[LoggingColumns.Patient.value].head(k))
+    print(results.true_positives[LoggingColumns.Patient.value])
 
     print_header(f"Top {k} true negatives")
-    print(results.true_negatives[LoggingColumns.Patient.value].head(k))
+    print(results.true_negatives[LoggingColumns.Patient.value])
--- Tests/ML/reports/test_classification_report.py
+++ Tests/ML/reports/test_classification_report.py
@@ -2,10 +2,15 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
+import numpy as np
+
 from pathlib import Path
 
 from InnerEye.Common.output_directories import TestOutputDirectories
 from InnerEye.ML.reports.notebook_report import generate_classification_notebook
+from InnerEye.ML.reports.classification_report import ReportedMetrics, get_results, get_metric, \
+    get_k_best_and_worst_performing, get_correct_and_misclassified_examples
+from InnerEye.ML.utils.metrics_constants import LoggingColumns
 
 
 def test_generate_classification_report(test_output_dirs: TestOutputDirectories) -> None:
@@ -20,3 +25,79 @@ def test_generate_classification_report(test_output_dirs: TestOutputDirectories)
     assert result_file.is_file()
     assert result_html.is_file()
     assert result_html.suffix == ".html"
+
+
+def test_get_results():
+    reports_folder = Path(__file__).parent
+    test_metrics_file = reports_folder / "test_metrics_classification.csv"
+
+    results = get_results(test_metrics_file)
+    assert all([results.subject_ids[i] == i for i in range(12)])
+    assert all([results.labels[i] == label for i, label in enumerate([1]*6 + [0]*6)])
+    assert all([results.model_outputs[i] == op for i, op in enumerate([0.0, 0.2, 0.4, 0.6, 0.8, 1.0] * 2)])
+
+
+def test_get_metric():
+    reports_folder = Path(__file__).parent
+    test_metrics_file = reports_folder / "test_metrics_classification.csv"
+    val_metrics_file = reports_folder / "val_metrics_classification.csv"
+
+    optimal_threshold = get_metric(test_metrics_csv=test_metrics_file,
+                                   val_metrics_csv=val_metrics_file,
+                                   metric=ReportedMetrics.OptimalThreshold)
+
+    assert optimal_threshold == 0.6
+
+    auc_roc = get_metric(test_metrics_csv=test_metrics_file,
+                         val_metrics_csv=val_metrics_file,
+                         metric=ReportedMetrics.AUC_ROC)
+    assert auc_roc == 0.5
+
+    auc_pr = get_metric(test_metrics_csv=test_metrics_file,
+                        val_metrics_csv=val_metrics_file,
+                        metric=ReportedMetrics.AUC_PR)
+
+    assert auc_pr == 0.504
+
+
+def test_get_correct_and_misclassified_examples():
+    reports_folder = Path(__file__).parent
+    test_metrics_file = reports_folder / "test_metrics_classification.csv"
+    val_metrics_file = reports_folder / "val_metrics_classification.csv"
+
+    results = get_correct_and_misclassified_examples(val_metrics_csv=val_metrics_file,
+                                                     test_metrics_csv=test_metrics_file)
+
+    true_positives = [item[LoggingColumns.Patient.value] for _, item in results.true_positives.iterrows()]
+    assert all([i in true_positives for i in [3, 4, 5]])
+
+    true_negatives = [item[LoggingColumns.Patient.value] for _, item in results.true_negatives.iterrows()]
+    assert all([i in true_negatives for i in [6, 7, 8]])
+
+    false_positives = [item[LoggingColumns.Patient.value] for _, item in results.false_positives.iterrows()]
+    assert all([i in false_positives for i in [9, 10, 11]])
+
+    false_negatives = [item[LoggingColumns.Patient.value] for _, item in results.false_negatives.iterrows()]
+    assert all([i in false_negatives for i in [0, 1, 2]])
+
+
+def test_get_k_best_and_worst_performing():
+    reports_folder = Path(__file__).parent
+    test_metrics_file = reports_folder / "test_metrics_classification.csv"
+    val_metrics_file = reports_folder / "val_metrics_classification.csv"
+
+    results = get_k_best_and_worst_performing(val_metrics_csv=val_metrics_file,
+                                              test_metrics_csv=test_metrics_file,
+                                              k=2)
+
+    best_true_positives = [item[LoggingColumns.Patient.value] for _, item in results.true_positives.iterrows()]
+    assert best_true_positives == [5, 4]
+
+    best_true_negatives = [item[LoggingColumns.Patient.value] for _, item in results.true_negatives.iterrows()]
+    assert best_true_negatives == [6, 7]
+
+    worst_false_positives = [item[LoggingColumns.Patient.value] for _, item in results.false_positives.iterrows()]
+    assert worst_false_positives == [11, 10]
+
+    worst_false_negatives = [item[LoggingColumns.Patient.value] for _, item in results.false_negatives.iterrows()]
+    assert worst_false_negatives == [0, 1]
