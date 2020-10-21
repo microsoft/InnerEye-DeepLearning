f7c58a398d63c6ccbc119281503e52843f496448
Melissa Bristow <mebristo@microsoft.com>
2020-10-21 15:42:27 +0000

    temporarily make val set smaller for debugging

--- InnerEye/ML/configs/segmentation/ProstateBase.py
+++ InnerEye/ML/configs/segmentation/ProstateBase.py
@@ -68,6 +68,6 @@ class ProstateBase(SegmentationModelBase):
         """
         Return an adjusted split
         """
-        return DatasetSplits.from_proportions(dataset_df, proportion_train=0.8, proportion_val=0.05,
-                                              proportion_test=0.15,
+        return DatasetSplits.from_proportions(dataset_df, proportion_train=0.94, proportion_val=0.05,
+                                              proportion_test=0.01,
                                               random_seed=0)
