9c27274ce4cef4357a92a74fef2d1ee0fdd3c722
Anton Schwaighofer <antonsc@microsoft.com>
2020-10-22 14:05:06 +0100

    flake

--- Tests/ML/util.py
+++ Tests/ML/util.py
@@ -3,9 +3,9 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 import logging
+import os
 from pathlib import Path
 from typing import Any, List, Optional, Union
-import os
 
 import numpy as np
 import pytest
