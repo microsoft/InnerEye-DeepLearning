85693a262ccd3329b8b2946ba097e6b412b0009a
Anton Schwaighofer <antonsc@microsoft.com>
2021-01-28 12:27:49 +0000

    docu update

--- InnerEye/Azure/azure_config.py
+++ InnerEye/Azure/azure_config.py
@@ -69,7 +69,8 @@ class AzureConfig(GenericConfig):
     model: str = param.String(doc="The name of the model to train/test.")
     only_register_model: bool = param.Boolean(False,
                                               doc="If set, and run_recovery_id is also set, register the model "
-                                                  "that was trained in the recovery run, but don't do training.")
+                                                  "that was trained in the recovery run, but don't do training"
+                                                  "or inference.")
     pytest_mark: str = param.String(doc="If provided, run pytest instead of model training. pytest will only "
                                         "run the tests that have the mark given in this argument "
                                         "('--pytest_mark gpu' will run all tests marked with 'pytest.mark.gpu')")
