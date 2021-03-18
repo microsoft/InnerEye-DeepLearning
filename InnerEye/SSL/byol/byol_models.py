dfc89ba79064f87ab9425d55abc1cf800898fff5
Ozan Oktay <ozan.oktay@microsoft.com>
2021-03-18 17:16:04 +0000

    add more docstrings

--- InnerEye/SSL/byol/byol_models.py
+++ InnerEye/SSL/byol/byol_models.py
@@ -10,15 +10,24 @@ from InnerEye.SSL.utils import create_ssl_encoder
 
 
 class _MLP(nn.Module):
-    def __init__(self, input_dim: int, hidden_size: int, output_dim: int) -> None:
+    """
+    Fully connected layers to map between image embeddings and project space where pairs of images are compared.
+    """
+
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int) -> None:
+        """
+        :param input_dim: Input embedding feature size 
+        :param hidden_dim: Hidden layer size in MLP
+        :param output_dim: Output projection size
+        """
         super().__init__()
         self.output_dim = output_dim
         self.input_dim = input_dim
         self.model = nn.Sequential(
-            nn.Linear(input_dim, hidden_size, bias=False),
-            nn.BatchNorm1d(hidden_size),
+            nn.Linear(input_dim, hidden_dim, bias=False),
+            nn.BatchNorm1d(hidden_dim),
             nn.ReLU(inplace=True),
-            nn.Linear(hidden_size, output_dim, bias=True))
+            nn.Linear(hidden_dim, output_dim, bias=True))
 
     def forward(self, x: T) -> T:
         x = self.model(x)
@@ -54,12 +63,16 @@ class SSLEncoder(nn.Module):
         return get_encoder_output_dim(self)
 
 class SiameseArm(nn.Module):
+    """
+    Implements the image encoder (f), projection (g) and predictor (q) modules used in BYOL.
+    """
+
     def __init__(self, *encoder_kwargs: Any) -> None:
         super().__init__()
 
         self.encoder = SSLEncoder(*encoder_kwargs)  # Encoder
-        self.projector = _MLP(input_dim=self.encoder.get_output_feature_dim(), hidden_size=2048, output_dim=128)
-        self.predictor = _MLP(input_dim=self.projector.output_dim, hidden_size=128, output_dim=128)
+        self.projector = _MLP(input_dim=self.encoder.get_output_feature_dim(), hidden_dim=2048, output_dim=128)
+        self.predictor = _MLP(input_dim=self.projector.output_dim, hidden_dim=128, output_dim=128)
         self.projector_normalised = nn.Sequential(self.projector,
                                                   Lambda(lambda x: nn.functional.normalize(x, dim=-1)))
 
