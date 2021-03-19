6855ca83cdc4fad66db935cac4432f354772a693
Shruthi42 <13177030+Shruthi42@users.noreply.github.com>
2021-03-19 19:39:30 +0530

    Import pytest

--- Tests/ML/reports/test_classification_multilabel_report.py
+++ Tests/ML/reports/test_classification_multilabel_report.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import pandas as pd
 import numpy as np
+import pytest
 
 from InnerEye.Common.metrics_constants import LoggingColumns
 from InnerEye.Common.output_directories import OutputFolderForTests
