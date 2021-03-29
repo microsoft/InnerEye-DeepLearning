73b79ac37f775a58b1971224eceb4b5ec22fb2b2
melanibe <melanie.bernhardt@microsoft.com>
2021-03-29 09:30:54 +0200

    Cleanup

--- InnerEye/SSL/datamodules/utils.py
+++ InnerEye/SSL/datamodules/utils.py
@@ -2,18 +2,15 @@ import multiprocessing
 from pathlib import Path
 from typing import Optional
 
-import torch
 import pytorch_lightning as pl
-from pl_bolts.datamodules import CIFAR10DataModule
+
 from .chestxray_datamodule import RSNAKaggleDataModule
 from .cifar_ie_datamodule import CIFARIEDataModule
 from ..configs.config_node import ConfigNode
 
-num_gpus = torch.cuda.device_count()
-num_devices = num_gpus if num_gpus > 0 else 1
 
-
-def create_ssl_data_modules(config: ConfigNode, dataset_path: Optional[Path]) -> pl.LightningDataModule:
+def create_ssl_data_modules(config: ConfigNode, dataset_path: Optional[Path],
+                            num_devices: int) -> pl.LightningDataModule:
     """
     Returns torch lightning data module.
     """
