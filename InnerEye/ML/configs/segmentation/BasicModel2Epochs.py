ec99744041f1c6aae803f294ae185c45c0d006c5
Javier Alvarez-Valle <jaalvare@microsoft.com>
2021-04-12 13:20:39 +0100

    Fix variable name

--- InnerEye/ML/configs/segmentation/BasicModel2Epochs.py
+++ InnerEye/ML/configs/segmentation/BasicModel2Epochs.py
@@ -27,7 +27,7 @@ class BasicModel2Epochs(SegmentationModelBase):
             ground_truth_ids_display_names=fg_classes,
             colours=[(255, 255, 255)] * len(fg_classes),
             fill_holes=[False] * len(fg_classes),
-            roi_intepreted_types=["ORGAN"] * len(self.fg_ids),
+            roi_intepreted_types=["ORGAN"] * len(self.fg_classes),
             mask_id="heart",
             norm_method=PhotometricNormalizationMethod.CtWindow,
             level=50,
