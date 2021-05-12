18d2c637c6acc1fc3587d0fdeb2ced1d84694107
Anton Schwaighofer <antonsc@microsoft.com>
2021-05-12 15:10:44 +0100

    merge

--- InnerEye/ML/runner.py
+++ InnerEye/ML/runner.py
@@ -264,7 +264,8 @@ class Runner:
             upload_timeout_seconds=86400,
         )
         source_config.set_script_params_except_submit_flag()
-        azure_run = submit_to_azureml(self.azure_config, source_config, self.azure_dataset_id, self.extra_azure_dataset_ids)
+        azure_run = submit_to_azureml(self.azure_config, source_config, self.azure_dataset_id,
+                                      self.extra_azure_dataset_ids)
         logging.info("Job submission to AzureML done.")
         if self.azure_config.pytest_mark and self.azure_config.wait_for_completion:
             # The AzureML job can optionally run pytest. Attempt to download it to the current directory.
