ffe3f6c1a525508d6be2b718b036bf554663bbaa
melanibe <mebernha@microsoft.com>
2021-05-24 12:13:39 +0100

    Make test for elastic human checkable

--- Tests/ML/augmentations/test_image_transforms.py
+++ Tests/ML/augmentations/test_image_transforms.py
@@ -8,35 +8,42 @@ import numpy as np
 import pytest
 
 import torch
+from PIL import Image
 
 from scipy.ndimage import gaussian_filter, map_coordinates
+from torchvision.transforms.functional import to_pil_image, to_tensor
 
+from InnerEye.Common.fixed_paths_for_tests import full_ml_test_data_path
 from InnerEye.ML.augmentations.image_transforms import AddGaussianNoise, ElasticTransform, ExpandChannels, RandomGamma
 
+
+image_size = (256, 256)
+
+test_tensor_1channel_1slice = torch.ones([1, 1, *image_size], dtype=torch.float)
+test_tensor_1channel_1slice[..., 100:150, 100:200] = 1 / 255.
+min_test_image, max_test_image = test_tensor_1channel_1slice.min(), test_tensor_1channel_1slice.max()
+test_tensor_2channels_2slices = torch.ones([2, 2, *image_size], dtype=torch.float)
+test_tensor_2channels_2slices[..., 100:150, 100:200] = 1 / 255.
+invalid_test_tensor = torch.ones([1, *image_size])
+test_pil_image = Image.open(str(full_ml_test_data_path() / "image_and_contour.png")).convert("RGB")
+test_image_as_tensor = to_tensor(test_pil_image).unsqueeze(0) # put in a [1, C, H, W] format
 
 def test_add_gaussian_noise() -> None:
     """
     Tests functionality of add gaussian noise
     """
-    np.random.seed(1)
-    tensor_img = torch.ones([1, 1, 256, 256], dtype=torch.float)
-    tensor_img[..., 100:150, 100:200] = 1 / 255.
-    # Input tensor [C, Z, H, W]
     torch.manual_seed(10)
-    transformed = AddGaussianNoise(std=0.05, p_apply=1)(tensor_img)
+    transformed = AddGaussianNoise(std=0.05, p_apply=1)(test_tensor_1channel_1slice.clone())
     torch.manual_seed(10)
     noise = torch.randn(size=(1, 256, 256)) * 0.05
-    assert torch.isclose(torch.clamp(tensor_img + noise, tensor_img.min(), tensor_img.max()),  # type: ignore
+    assert torch.isclose(torch.clamp(test_tensor_1channel_1slice + noise, min_test_image, max_test_image),  # type: ignore
                          transformed).all()
     # Check that it applies the same transform to all slices
-    tensor_img = torch.ones([2, 2, 256, 256], dtype=torch.float)
-    tensor_img[..., 100:150, 100:200] = 1 / 255.
-    # Input tensor [C, Z, H, W]
     torch.manual_seed(10)
-    transformed = AddGaussianNoise(std=0.05, p_apply=1)(tensor_img)
+    transformed = AddGaussianNoise(std=0.05, p_apply=1)(test_tensor_2channels_2slices.clone())
     torch.manual_seed(10)
     noise = torch.randn(size=(1, 256, 256)) * 0.05
-    assert torch.isclose(torch.clamp(tensor_img + noise, tensor_img.min(), tensor_img.max()),  # type: ignore
+    assert torch.isclose(torch.clamp(test_tensor_2channels_2slices + noise, min_test_image, max_test_image),  # type: ignore
                          transformed).all()
 
 
@@ -44,54 +51,35 @@ def test_elastic_transform() -> None:
     """
     Tests elastic transform
     """
-    image = torch.ones([2, 2, 256, 256]) * 255.
-    image[..., 100:150, 100:200] = 1
-
-    # Computed expected transform
     np.random.seed(7)
-    np.random.random(1)
-
-    shape = (256, 256)
-    dx = gaussian_filter((np.random.random(shape) * 2 - 1), 4, mode="constant", cval=0) * 34
-    dy = gaussian_filter((np.random.random(shape) * 2 - 1), 4, mode="constant", cval=0) * 34
-    all_dimensions_axes = [np.arange(dim) for dim in image.shape]
-    grid = np.meshgrid(*all_dimensions_axes, indexing='ij')
-    grid[-2] = grid[-2] + dx
-    grid[-1] = grid[-1] + dy
-    indices = [np.reshape(grid[i], (-1, 1)) for i in range(len(grid))]
-    expected_tensor = torch.tensor(map_coordinates(image, indices, order=1).reshape(image.shape))
-    # Actual transform
-    np.random.seed(7)
-    transformed_image = ElasticTransform(sigma=4, alpha=34, p_apply=1.0)(image)
-    assert torch.isclose(expected_tensor, transformed_image).all()
+    transformed_image = ElasticTransform(sigma=4, alpha=34, p_apply=1.0)(test_image_as_tensor.clone())
+    transformed_pil = to_pil_image(transformed_image.squeeze(0))
+    expected_pil_image = Image.open(full_ml_test_data_path() / "elastic_transformed_image_and_contour.png").convert("RGB")
+    assert expected_pil_image == transformed_pil
 
 
 def test_expand_channels() -> None:
-    tensor_img = torch.ones([1, 256, 256])
     with pytest.raises(ValueError):
-        ExpandChannels()(tensor_img)
+        ExpandChannels()(invalid_test_tensor)
 
-    tensor_img = torch.ones([1, 1, 256, 256])
-    tensor_img = ExpandChannels()(tensor_img)
+    tensor_img = ExpandChannels()(test_tensor_1channel_1slice.clone())
     assert tensor_img.shape == torch.Size([1, 3, 256, 256])
 
 
 def test_random_gamma() -> None:
     # This is invalid input (expects 4 dimensions)
-    tensor_img = torch.ones([1, 256, 256])
     with pytest.raises(ValueError):
-        RandomGamma(scale=(0.3, 3))(tensor_img)
+        RandomGamma(scale=(0.3, 3))(invalid_test_tensor)
 
     random.seed(0)
-    tensor_img = torch.ones([1, 1, 256, 256])
-    transformed_1 = RandomGamma(scale=(0.3, 3))(tensor_img)
-    assert transformed_1.shape == torch.Size([1, 1, 256, 256])
+    transformed_1 = RandomGamma(scale=(0.3, 3))(test_tensor_1channel_1slice.clone())
+    assert transformed_1.shape == test_tensor_1channel_1slice.shape
 
     random.seed(0)
-    tensor_img = torch.ones([2, 3, 256, 256])
+    tensor_img = torch.ones([2, 3, *image_size])
     transformed_2 = RandomGamma(scale=(0.3, 3))(tensor_img)
     # If you run on 1 channel, 1 Z dimension the gamma transform applied should be the same for all slices.
-    assert transformed_2.shape == torch.Size([2, 3, 256, 256])
+    assert transformed_2.shape == torch.Size([2, 3, *image_size])
     assert torch.isclose(transformed_2[0], transformed_2[1]).all()
     assert torch.isclose(transformed_2[0, 1], transformed_2[0, 2]).all() and \
            torch.isclose(transformed_2[0, 0], transformed_2[0, 2]).all()
