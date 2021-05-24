20502ca2d63509f10ccc9728aa8c1ca82521e151
melanibe <mebernha@microsoft.com>
2021-05-24 13:44:48 +0100

    Solve OOM, remove duplicate test input definitions

--- Tests/ML/augmentations/test_transform_pipeline.py
+++ Tests/ML/augmentations/test_transform_pipeline.py
@@ -21,6 +21,18 @@ from Tests.SSL.test_data_modules import cxr_augmentation_config
 
 import numpy as np
 
+image_size = (32, 32)
+crop_size = 24
+test_image_as_array = np.ones(list(image_size)) * 255.
+test_image_as_array[10:15, 10:20] = 1
+test_image_as_pil = PIL.Image.fromarray(test_image_as_array).convert("L")
+test_2d_image_as_CHW_tensor = to_tensor(test_image_as_array)
+
+test_2d_image_as_ZCHW_tensor = test_2d_image_as_CHW_tensor.unsqueeze(0)
+
+test_4d_scan_as_tensor = torch.ones([5, 4, *image_size]) * 255.
+test_4d_scan_as_tensor[..., 10:15, 10:20] = 1
+
 
 @pytest.mark.parametrize("use_different_transformation_per_channel", [True, False])
 def test_torchvision_on_various_input(use_different_transformation_per_channel: bool) -> None:
@@ -29,41 +41,38 @@ def test_torchvision_on_various_input(use_different_transformation_per_channel: 
     of input: PIL image, 3D tensor, 4D tensors. Tests that use_different_transformation_per_channel has the correct
     behavior.
     """
-    image = np.ones([256, 256]) * 255.
-    image[100:150, 100:200] = 1
+
     transform = ImageTransformationPipeline(
-        [CenterCrop(224),
+        [CenterCrop(crop_size),
          RandomErasing(),
          RandomAffine(degrees=(10, 12), shear=15, translate=(0.1, 0.3))
          ],
         use_different_transformation_per_channel)
 
     # Test PIL image input
-    image = PIL.Image.fromarray(image).convert("L")
-    transform(image)
+    transformed = transform(test_image_as_pil)
+    assert isinstance(transformed, torch.Tensor)
+    assert transformed.shape == torch.Size([1, crop_size, crop_size])
 
     # Test image as [C, H. W] tensor
-    image_as_tensor = to_tensor(image)
-    transform(image_as_tensor)
+    transformed = transform(test_2d_image_as_CHW_tensor.clone())
+    assert isinstance(transformed, torch.Tensor)
+    assert transformed.shape == torch.Size([1, crop_size, crop_size])
 
     # Test image as [1, 1, H, W]
-    image_as_tensor = image_as_tensor.unsqueeze(0)
-    assert image_as_tensor.shape == torch.Size([1, 1, 256, 256])
-    transformed = transform(image_as_tensor)
+    transformed = transform(test_2d_image_as_ZCHW_tensor)
     assert isinstance(transformed, torch.Tensor)
-    assert transformed.shape == torch.Size([1, 1, 224, 224])
+    assert transformed.shape == torch.Size([1, 1, crop_size, crop_size])
 
-    # Test with a fake scan [C, Z, H, W] -> [25, 34, 256, 256]
-    test_4d_tensor = torch.ones([25, 34, 256, 256]) * 255.
-    test_4d_tensor[..., 100:150, 100:200] = 1
-    transformed = transform(test_4d_tensor)
+    # Test with a fake 4D scan [C, Z, H, W] -> [25, 34, 32, 32]
+    transformed = transform(test_4d_scan_as_tensor)
     assert isinstance(transformed, torch.Tensor)
-    assert transformed.shape == torch.Size([25, 34, 224, 224])
+    assert transformed.shape == torch.Size([5, 4, crop_size, crop_size])
 
     # Same transformation should be applied to all slices and channels.
     assert torch.isclose(transformed[0, 0], transformed[1, 1]).all() != use_different_transformation_per_channel
 
-@pytest.mark.skipif(is_windows(), reason="OOM on windows")
+
 @pytest.mark.parametrize("use_different_transformation_per_channel", [True, False])
 def test_custom_tf_on_various_input(use_different_transformation_per_channel: bool) -> None:
     """
@@ -71,8 +80,6 @@ def test_custom_tf_on_various_input(use_different_transformation_per_channel: bo
     of input: PIL image, 3D tensor, 4D tensors. Tests that use_different_transformation_per_channel has the correct
     behavior. The transforms are test individually in test_image_transforms.py
     """
-    image = np.ones([256, 256]) * 255.
-    image[100:150, 100:200] = 1
     pipeline = ImageTransformationPipeline(
         [ElasticTransform(sigma=4, alpha=34, p_apply=1),
          AddGaussianNoise(p_apply=1, std=0.05),
@@ -81,26 +88,22 @@ def test_custom_tf_on_various_input(use_different_transformation_per_channel: bo
         use_different_transformation_per_channel)
 
     # Test PIL image input
-    image = PIL.Image.fromarray(image).convert("L")
-    pipeline(image)
+    transformed = pipeline(test_image_as_pil)
+    assert transformed.shape == test_2d_image_as_CHW_tensor.shape
 
     # Test image as [C, H, W] tensor
-    image_as_tensor = to_tensor(image)
-    pipeline(image_as_tensor)
+    pipeline(test_2d_image_as_CHW_tensor)
+    assert transformed.shape == test_2d_image_as_CHW_tensor.shape
 
     # Test image as [1, 1, H, W]
-    image_as_tensor = image_as_tensor.unsqueeze(0)
-    assert image_as_tensor.shape == torch.Size([1, 1, 256, 256])
-    transformed = pipeline(image_as_tensor)
+    transformed = pipeline(test_2d_image_as_ZCHW_tensor)
     assert isinstance(transformed, torch.Tensor)
-    assert transformed.shape == torch.Size([1, 1, 256, 256])
+    assert transformed.shape == torch.Size([1, 1, *image_size])
 
-    # Test with a fake scan [C, Z, H, W] -> [25, 34, 256, 256]
-    test_4d_tensor = torch.ones([25, 34, 256, 256]) * 255.
-    test_4d_tensor[..., 100:150, 100:200] = 1
-    transformed = pipeline(test_4d_tensor)
+    # Test with a fake scan [C, Z, H, W] -> [25, 34, 32, 32]
+    transformed = pipeline(test_4d_scan_as_tensor)
     assert isinstance(transformed, torch.Tensor)
-    assert transformed.shape == torch.Size([25, 34, 256, 256])
+    assert transformed.shape == test_4d_scan_as_tensor.shape
 
     # Same transformation should be applied to all slices and channels.
     assert torch.isclose(transformed[0, 0], transformed[1, 1]).all() != use_different_transformation_per_channel
@@ -111,9 +114,9 @@ def test_create_transform_pipeline_from_config() -> None:
     Tests that the pipeline returned by create_transform_pipeline_from_config returns the expected transformation.
     """
     transformation_pipeline = create_cxr_transforms_from_config(cxr_augmentation_config, apply_augmentations=True)
-    image = np.ones([256, 256]) * 255.
-    image[100:150, 100:200] = 1
-    image = PIL.Image.fromarray(image).convert("L")
+    fake_cxr_as_array = np.ones([256, 256]) * 255.
+    fake_cxr_as_array[100:150, 100:200] = 1
+    fake_cxr_image = PIL.Image.fromarray(fake_cxr_as_array).convert("L")
 
     all_transforms = [ExpandChannels(),
                       RandomAffine(degrees=180, translate=(0, 0), shear=40),
@@ -131,7 +134,7 @@ def test_create_transform_pipeline_from_config() -> None:
     torch.manual_seed(3)
     random.seed(3)
 
-    transformed_image = transformation_pipeline(image)
+    transformed_image = transformation_pipeline(fake_cxr_image)
     assert isinstance(transformed_image, torch.Tensor)
     # Expected pipeline
     image = np.ones([256, 256]) * 255.
