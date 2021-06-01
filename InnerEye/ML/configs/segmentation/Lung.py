1837c3f98a55a7f825e4dbf49aefae2492be1c14
Javier Alvarez-Valle <jaalvare@microsoft.com>
2021-06-01 22:07:35 +0100

    Add default hyperdrive config

--- InnerEye/ML/configs/segmentation/Lung.py
+++ InnerEye/ML/configs/segmentation/Lung.py
@@ -3,11 +3,14 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 from typing import Any
-
+from azureml.train.hyperdrive import BayesianParameterSampling, NoTerminationPolicy, choice
 import numpy
 import pandas as pd
+from azureml.core import ScriptRunConfig
+from azureml.train.hyperdrive import HyperDriveConfig, PrimaryMetricGoal
 
-from InnerEye.ML.config import MixtureLossComponent, PhotometricNormalizationMethod, SegmentationLoss, \
+from InnerEye.Common.metrics_constants import TrackedMetrics
+from InnerEye.ML.config import PhotometricNormalizationMethod, SegmentationLoss, \
     SegmentationModelBase, equally_weighted_classes
 from InnerEye.ML.deep_learning_config import OptimizerType
 from InnerEye.ML.utils.split_dataset import DatasetSplits
@@ -39,10 +42,10 @@ class Lung(SegmentationModelBase):
             fill_holes=[False] * len(fg_classes),
             roi_interpreted_types=["ORGAN"] * len(fg_classes),
             largest_connected_component_foreground_classes=["lung_r", "lung_l", "heart"],
-            num_dataload_workers=8,
+            num_dataload_workers=0,
             norm_method=PhotometricNormalizationMethod.CtWindow,
-            level=40,
-            window=400,
+            level=0,
+            window=4000,
             class_weights=equally_weighted_classes(fg_classes, background_weight=0.02),
             train_batch_size=8,
             inference_batch_size=1,
@@ -51,6 +54,7 @@ class Lung(SegmentationModelBase):
             l_rate=1e-3,
             min_l_rate=1e-5,
             l_rate_polynomial_gamma=0.9,
+            check_exclusive=False,
             optimizer_type=OptimizerType.Adam,
             opt_eps=1e-4,
             adam_betas=(0.9, 0.999),
@@ -60,9 +64,7 @@ class Lung(SegmentationModelBase):
             use_mixed_precision=True,
             use_model_parallel=True,
             monitoring_interval_seconds=0,
-            loss_type=SegmentationLoss.Mixture,
-            mixture_loss_components=[MixtureLossComponent(0.5, SegmentationLoss.Focal, 0.2),
-                                     MixtureLossComponent(0.5, SegmentationLoss.SoftDice, 0.1)],
+            loss_type=SegmentationLoss.SoftDice,
         )
         self.add_and_validate(kwargs)
 
@@ -79,4 +81,39 @@ class Lung(SegmentationModelBase):
             test_ids=test,
             val_ids=val,
             train_ids=train
+        )
+
+    def get_parameter_search_hyperdrive_config(self, run_config: ScriptRunConfig) -> HyperDriveConfig:
+        """
+        Specify an Azure HyperDrive configuration.
+        Further details are described in the tutorial
+        https://docs.microsoft.com/en-us/azure/machine-learning/service/how-to-tune-hyperparameters
+        A reference is provided at https://docs.microsoft.com/en-us/python/api/azureml-train-core/azureml.train
+        .hyperdrive?view=azure-ml-py
+        :param run_config: The configuration for running an individual experiment.
+        :return: An Azure HyperDrive run configuration (configured PyTorch environment).
+        """
+        crop_sizes = [f'{z}, {x}, {x}' for x in range(128, 512, 32) for z in [64, 112]]
+
+        parameter_space = {
+            'crop_size': choice(crop_sizes),
+            'feature_channels': choice(16, 32),
+            'l_rate_step_step_size': choice(20, 40, 75),
+            'l_rate': choice(0.0005, 0.001, 0.01),
+            'train_batch_size': choice(1, 2, 4, 8, 16, 32),
+            'num_epochs': choice(50, 80, 150),
+            'loss_type': choice("SoftDice", "Focal")
+        }
+
+        param_sampling = BayesianParameterSampling(parameter_space)
+
+        return HyperDriveConfig(
+            run_config=run_config,
+            hyperparameter_sampling=param_sampling,
+            policy=NoTerminationPolicy(),
+            primary_metric_name=TrackedMetrics.Val_Loss.value,
+            primary_metric_goal=PrimaryMetricGoal.MINIMIZE,
+            max_total_runs=12,
+            max_concurrent_runs=2,
+            max_duration_minutes=30,  # Max 2 hours per experiment
         )
