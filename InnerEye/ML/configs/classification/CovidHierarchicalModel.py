0f3478e70bb2bc46485c467aa2fd572621eb2427
Shruthi42 <13177030+Shruthi42@users.noreply.github.com>
2021-06-14 12:28:19 +0100

    Update function name

--- InnerEye/ML/configs/classification/CovidHierarchicalModel.py
+++ InnerEye/ML/configs/classification/CovidHierarchicalModel.py
@@ -83,8 +83,9 @@ class CovidHierarchicalModel(ScalarModelBase):
                          l_rate_scheduler=LRSchedulerType.Step,
                          l_rate_step_gamma=1.0,
                          l_rate_multi_step_milestones=None,
-                         **kwargs)
+                         should_validate=False)
         self.num_classes = 3
+        self.add_and_validate(kwargs)
 
     def validate(self) -> None:
         self.l_rate = 1e-5 if self.use_pretrained_model else 1e-4
@@ -132,7 +133,7 @@ class CovidHierarchicalModel(ScalarModelBase):
                                                   shuffle=True)
 
     # noinspection PyTypeChecker
-    def get_image_sample_transforms(self) -> ModelTransformsPerExecutionMode:
+    def get_image_transform(self) -> ModelTransformsPerExecutionMode:
         config = load_yaml_augmentation_config(path_linear_head_augmentation_cxr)
         train_transforms = Compose(
             [DicomPreparation(), create_cxr_transforms_from_config(config, apply_augmentations=True)])
