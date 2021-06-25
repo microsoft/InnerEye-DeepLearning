36f39949e45017fb2a71926b86ffa86bcdeadf3a
Tim Regan <timregan@microsoft.com>
2021-06-25 20:47:22 +0100

    WiP testing partial_ground_truth metrics output

--- Tests/ML/test_model_testing.py
+++ Tests/ML/test_model_testing.py
@@ -30,7 +30,8 @@ from Tests.ML.utils.test_model_util import create_model_and_store_checkpoint
 
 
 @pytest.mark.skipif(common_util.is_windows(), reason="Too slow on windows")
-def test_model_test(test_output_dirs: OutputFolderForTests) -> None:
+@pytest.mark.parametrize("partial_ground_truth", [True, False])
+def test_model_test(test_output_dirs: OutputFolderForTests, partial_ground_truth: bool) -> None:
     train_and_test_data_dir = full_ml_test_data_path("train_and_test_data")
     seed_everything(42)
     config = DummyModel()
@@ -40,12 +41,16 @@ def test_model_test(test_output_dirs: OutputFolderForTests) -> None:
     transform = config.get_full_image_sample_transforms().test
     df = pd.read_csv(full_ml_test_data_path(DATASET_CSV_FILE_NAME))
     df = df[df.subject.isin([1, 2])]
+    if partial_ground_truth:
+        # Patient 1 has one missing ground truth channel: "region"
+        config.check_exclusive = False
+        config.ground_truth_ids = ["region", "region_1"]
+        df = df[df["subject"].ne(1) & df["channel"].ne("region")]
     # noinspection PyTypeHints
     config._datasets_for_inference = \
         {ModelExecutionMode.TEST: FullImageDataset(config, df, full_image_sample_transforms=transform)}  # type: ignore
     execution_mode = ModelExecutionMode.TEST
-    checkpoint_handler = get_default_checkpoint_handler(model_config=config,
-                                                        project_root=test_output_dirs.root_dir)
+    checkpoint_handler = get_default_checkpoint_handler(model_config=config, project_root=test_output_dirs.root_dir)
     # Mimic the behaviour that checkpoints are downloaded from blob storage into the checkpoints folder.
     create_model_and_store_checkpoint(config, config.checkpoint_folder / BEST_CHECKPOINT_FILE_NAME_WITH_SUFFIX)
     checkpoint_handler.additional_training_done()
@@ -53,60 +58,51 @@ def test_model_test(test_output_dirs: OutputFolderForTests) -> None:
                                                               data_split=execution_mode,
                                                               checkpoint_handler=checkpoint_handler)
     epoch_dir = config.outputs_folder / get_best_epoch_results_path(execution_mode)
-    assert inference_results.metrics == pytest.approx(0.66606902, abs=1e-6)
 
-    assert config.outputs_folder.is_dir()
-    assert epoch_dir.is_dir()
-    patient1 = io_util.load_nifti_image(train_and_test_data_dir / "id1_channel1.nii.gz")
-    patient2 = io_util.load_nifti_image(train_and_test_data_dir / "id2_channel1.nii.gz")
+    if partial_ground_truth:
+        raise NotImplementedError("soon")
+    else:
+        assert inference_results.metrics == pytest.approx(0.66606902, abs=1e-6)
+        assert config.outputs_folder.is_dir()
+        assert epoch_dir.is_dir()
+        patient1 = io_util.load_nifti_image(train_and_test_data_dir / "id1_channel1.nii.gz")
+        patient2 = io_util.load_nifti_image(train_and_test_data_dir / "id2_channel1.nii.gz")
 
-    assert_file_contains_string(epoch_dir / DATASET_ID_FILE, placeholder_dataset_id)
-    assert_file_contains_string(epoch_dir / GROUND_TRUTH_IDS_FILE, "region")
-    assert_text_files_match(epoch_dir / model_testing.SUBJECT_METRICS_FILE_NAME,
-                            train_and_test_data_dir / model_testing.SUBJECT_METRICS_FILE_NAME)
-    assert_text_files_match(epoch_dir / model_testing.METRICS_AGGREGATES_FILE,
-                            train_and_test_data_dir / model_testing.METRICS_AGGREGATES_FILE)
-    # Plotting results vary between platforms. Can only check if the file is generated, but not its contents.
-    assert (epoch_dir / model_testing.BOXPLOT_FILE).exists()
+        assert_file_contains_string(epoch_dir / DATASET_ID_FILE, placeholder_dataset_id)
+        assert_file_contains_string(epoch_dir / GROUND_TRUTH_IDS_FILE, "region")
+        assert_text_files_match(epoch_dir / model_testing.SUBJECT_METRICS_FILE_NAME,
+                                train_and_test_data_dir / model_testing.SUBJECT_METRICS_FILE_NAME)
+        assert_text_files_match(epoch_dir / model_testing.METRICS_AGGREGATES_FILE,
+                                train_and_test_data_dir / model_testing.METRICS_AGGREGATES_FILE)
+        # Plotting results vary between platforms. Can only check if the file is generated, but not its contents.
+        assert (epoch_dir / model_testing.BOXPLOT_FILE).exists()
 
-    assert_nifti_content(epoch_dir / "001" / "posterior_region.nii.gz", get_image_shape(patient1),
-                         patient1.header,
-                         [137], np.ubyte)
-    assert_nifti_content(epoch_dir / "002" / "posterior_region.nii.gz", get_image_shape(patient2),
-                         patient2.header,
-                         [137], np.ubyte)
-    assert_nifti_content(epoch_dir / "001" / DEFAULT_RESULT_IMAGE_NAME, get_image_shape(patient1),
-                         patient1.header,
-                         [1], np.ubyte)
-    assert_nifti_content(epoch_dir / "002" / DEFAULT_RESULT_IMAGE_NAME, get_image_shape(patient2),
-                         patient2.header,
-                         [1], np.ubyte)
-    assert_nifti_content(epoch_dir / "001" / "posterior_background.nii.gz", get_image_shape(patient1),
-                         patient1.header,
-                         [117], np.ubyte)
-    assert_nifti_content(epoch_dir / "002" / "posterior_background.nii.gz", get_image_shape(patient2),
-                         patient2.header,
-                         [117], np.ubyte)
-    thumbnails_folder = epoch_dir / model_testing.THUMBNAILS_FOLDER
-    assert thumbnails_folder.is_dir()
-    png_files = list(thumbnails_folder.glob("*.png"))
-    overlays = [f for f in png_files if "_region_slice_" in str(f)]
-    assert len(overlays) == len(df.subject.unique()), "There should be one overlay/contour file per subject"
+        assert_nifti_content(epoch_dir / "001" / "posterior_region.nii.gz", get_image_shape(patient1), patient1.header, [137], np.ubyte)
+        assert_nifti_content(epoch_dir / "002" / "posterior_region.nii.gz", get_image_shape(patient2), patient2.header, [137], np.ubyte)
+        assert_nifti_content(epoch_dir / "001" / DEFAULT_RESULT_IMAGE_NAME, get_image_shape(patient1), patient1.header, [1], np.ubyte)
+        assert_nifti_content(epoch_dir / "002" / DEFAULT_RESULT_IMAGE_NAME, get_image_shape(patient2), patient2.header, [1], np.ubyte)
+        assert_nifti_content(epoch_dir / "001" / "posterior_background.nii.gz", get_image_shape(patient1), patient1.header, [117], np.ubyte)
+        assert_nifti_content(epoch_dir / "002" / "posterior_background.nii.gz", get_image_shape(patient2), patient2.header, [117], np.ubyte)
+        thumbnails_folder = epoch_dir / model_testing.THUMBNAILS_FOLDER
+        assert thumbnails_folder.is_dir()
+        png_files = list(thumbnails_folder.glob("*.png"))
+        overlays = [f for f in png_files if "_region_slice_" in str(f)]
+        assert len(overlays) == len(df.subject.unique()), "There should be one overlay/contour file per subject"
 
-    # Writing dataset.csv normally happens at the beginning of training,
-    # but this test reads off a saved checkpoint file.
-    # Dataset.csv must be present for plot_cross_validation.
-    config.write_dataset_files()
-    # Test if the metrics files can be picked up correctly by the cross validation code
-    config_and_files = get_config_and_results_for_offline_runs(config)
-    result_files = config_and_files.files
-    assert len(result_files) == 1
-    for file in result_files:
-        assert file.execution_mode == execution_mode
-        assert file.dataset_csv_file is not None
-        assert file.dataset_csv_file.exists()
-        assert file.metrics_file is not None
-        assert file.metrics_file.exists()
+        # Writing dataset.csv normally happens at the beginning of training,
+        # but this test reads off a saved checkpoint file.
+        # Dataset.csv must be present for plot_cross_validation.
+        config.write_dataset_files()
+        # Test if the metrics files can be picked up correctly by the cross validation code
+        config_and_files = get_config_and_results_for_offline_runs(config)
+        result_files = config_and_files.files
+        assert len(result_files) == 1
+        for file in result_files:
+            assert file.execution_mode == execution_mode
+            assert file.dataset_csv_file is not None
+            assert file.dataset_csv_file.exists()
+            assert file.metrics_file is not None
+            assert file.metrics_file.exists()
 
 
 @pytest.mark.parametrize("config", [DummyModel(), ClassificationModelForTesting()])
