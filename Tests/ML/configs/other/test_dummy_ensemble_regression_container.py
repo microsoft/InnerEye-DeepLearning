2d75d24a3187682c0bb243660ea4f752df207c98
Tim Regan <timregan@microsoft.com>
2021-07-14 12:08:52 +0100

    WiP swapping back to checkpoints not accruing child runs

--- Tests/ML/configs/other/test_dummy_ensemble_regression_container.py
+++ Tests/ML/configs/other/test_dummy_ensemble_regression_container.py
@@ -4,10 +4,11 @@
 #  ------------------------------------------------------------------------------------------
 
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 from unittest import mock
 
 import numpy as np
+from numpy.lib.function_base import append
 from sklearn.model_selection import KFold
 import torch
 
@@ -61,33 +62,79 @@ def test_trained_ensemble(test_output_dirs: OutputFolderForTests) -> None:
     Make real checkpoints and load them, test ensemble gets better accuracy than any of the child cross validation
     models.
     """
-    runner = default_runner()
     local_dataset = test_output_dirs.root_dir / "dataset"
     local_dataset.mkdir()
-    args = ["", "--model=DummyEnsembleRegressionContainer", "--model_configs_namespace=Tests.ML.configs",
-            f"--output_to={test_output_dirs.root_dir}", f"--local_dataset={local_dataset}",
-            "--number_of_cross_validation_splits=0"]
-    # Since ross validation for Lightning models will not run locally in our infrastructure we need to set up the data
+    # Since cross validation for Lightning models will not run locally in our infrastructure we need to set up the data
     # manually for each of the cross validation runs.
     raw_data = np.loadtxt(
-        tests_root_directory().parent() / "InnerEye" / "ML" / "other" / "hellocontainer.csv",
+        tests_root_directory().parent / "InnerEye" / "ML" / "configs" / "other" / "hellocontainer.csv",
         delimiter=",")
     np.random.seed(42)
     np.random.shuffle(raw_data)
     test_data = raw_data[70:100]
     raw_data_remaining = raw_data[0:70]
     k_fold = KFold(n_splits=5)
+    checkpoint_paths: List[Path] = []
+    test_mses: List[float] = []
+    test_maes: List[float] = []
+    first_child: DummyEnsembleRegressionModule = None
     for cross_validation_split_index in range(5):
+        runner = default_runner()
+        local_dataset = test_output_dirs.root_dir / "dataset" / str(cross_validation_split_index)
+        local_dataset.mkdir()
+        args = ["", "--model=DummyEnsembleRegressionContainer", "--model_configs_namespace=Tests.ML.configs",
+            f"--output_to={test_output_dirs.root_dir}", f"--local_dataset={local_dataset}",
+            "--number_of_cross_validation_splits=0"]
         train_indexes, val_indexes = list(k_fold.split(raw_data_remaining))[cross_validation_split_index]
         train_data = raw_data_remaining[train_indexes]
         val_data = raw_data_remaining[val_indexes]
-        fold_data = np.append(train_data, [val_data, test_data])
-        fold_data.wr
+        fold_data = np.concatenate((train_data, val_data, test_data), axis=0)
+        np.savetxt(local_dataset / "hellocontainer.csv", fold_data, delimiter=",")
         with mock.patch("sys.argv", args):
-            loaded_config, actual_run = runner.run()
-    # Test if the outputs folder is relative to the folder that we specified via the commandline
-    runner.lightning_container.outputs_folder.relative_to(test_output_dirs.root_dir)
-    results = runner.lightning_container.outputs_folder
-    # Test if all the files that are written during inference exist.
-    assert not (results / "on_inference_start.txt").is_file()
-    assert (results / "test_step.txt").is_file()
+            loaded_config, _ = runner.run()
+        if not first_child:
+            first_child = loaded_config.created_model
+            first_child.siblings = [first_child]
+        else:
+            first_child.siblings.append(loaded_config.created_model)
+        checkpoint_path = Path(loaded_config.output_to) / "checkpoints" / "best_checkpoint.ckpt"
+        checkpoint_paths.append(checkpoint_path)
+        mse_metrics = _load_metrics(metrics_file=loaded_config.file_system_config.run_folder / "test_mse.txt")
+        test_mses.append(mse_metrics["TEST"])
+        mae_metrics = _load_metrics(metrics_file=loaded_config.file_system_config.run_folder / "test_mae.txt")
+        test_maes.append(mae_metrics["TEST"])
+        print("wait")
+    # Get test data split
+    data_module_xval = HelloDataModule(
+        root_folder=HelloContainer().local_dataset,  # type: ignore
+        cross_validation_split_index=0,
+        number_of_cross_validation_splits=5)
+    test_dataloader = data_module_xval.test_dataloader()
+    # Run inference loop
+    first_child.on_inference_start()
+    first_child.on_inference_start_dataset(execution_mode=ModelExecutionMode.TEST, is_ensemble_model=True)
+    for batch_idx, batch in enumerate(test_dataloader):
+        posteriors = first_child.forward(batch['x'])
+        first_child.record_posteriors(batch, batch_idx, posteriors)
+    first_child.on_inference_end_dataset()
+    mse_metrics = _load_metrics(metrics_file=first_child.file_system_config.run_folder / "test_mse.txt")
+    test_mse = mse_metrics["TEST"]
+    mae_metrics = _load_metrics(metrics_file=loaded_config.file_system_config.run_folder / "test_mae.txt")
+    test_mae = mae_metrics["TEST"]
+    print("wait")
+
+def _load_metrics(metrics_file: Path) -> Dict[str, float]:
+    """
+    Load the metrics for each execution mode present in the file saved during inference.
+    :param metrics_file_path: The path to the metrics file saved during inference.
+    :returns: MAp between the execution mode string and the metric's values found in the file.
+    """
+    text = metrics_file.read_text()
+    metrics: Dict[str, float] = {}
+    for line in text.split('\n'):
+        if line:
+            splits = line.split(": ")
+            execution_mode_string = splits[0]
+            metrics_value = float(splits[1])
+            metrics[execution_mode_string] = metrics_value
+    return metrics
