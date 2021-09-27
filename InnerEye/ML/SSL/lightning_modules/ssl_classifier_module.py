1375753364c958cab8e8a5d09b064b55ad500ad4
Anton Schwaighofer <antonsc@microsoft.com>
2021-09-27 15:17:31 +0100

    more logging cleanup

--- InnerEye/ML/SSL/lightning_modules/ssl_classifier_module.py
+++ InnerEye/ML/SSL/lightning_modules/ssl_classifier_module.py
@@ -5,11 +5,12 @@
 from typing import Any, List, Optional
 
 import torch
-from torchmetrics import Metric
 from pl_bolts.models.self_supervised import SSLEvaluator
 from torch.nn import functional as F
+from torchmetrics import Metric
 
 from InnerEye.ML.SSL.encoders import get_encoder_output_dim
+from InnerEye.ML.SSL.utils import log_on_epoch
 from InnerEye.ML.dataset.scalar_sample import ScalarItem
 from InnerEye.ML.lightning_container import LightningModuleWithOptimizer
 from InnerEye.ML.lightning_metrics import Accuracy05, AreaUnderPrecisionRecallCurve, AreaUnderRocCurve
@@ -79,16 +80,16 @@ class SSLClassifier(LightningModuleWithOptimizer, DeviceAwareModule):
 
     def training_step(self, batch: Any, batch_id: int, *args: Any, **kwargs: Any) -> Any:  # type: ignore
         loss = self.shared_step(batch, True)
-        self.log("train/loss", loss, on_step=False, on_epoch=True)
+        log_on_epoch(self, {"train/loss": loss})
         for metric in self.train_metrics:
-            self.log(f"train/{metric.name}", metric, on_epoch=True, on_step=False)
+            log_on_epoch(self, {f"train/{metric.name}": metric})
         return loss
 
     def validation_step(self, batch: Any, batch_id: int, *args: Any, **kwargs: Any) -> None:  # type: ignore
         loss = self.shared_step(batch, is_training=False)
-        self.log('val/loss', loss, on_step=False, on_epoch=True, sync_dist=True)
+        log_on_epoch(self, {'val/loss': loss})
         for metric in self.val_metrics:
-            self.log(f"val/{metric.name}", metric, on_epoch=True, on_step=False)
+            log_on_epoch(self, {f"val/{metric.name}": metric})
 
     def get_input_tensors(self, item: ScalarItem) -> List[torch.Tensor]:
         """
