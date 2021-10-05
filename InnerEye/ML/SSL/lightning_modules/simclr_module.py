92cfccb640cf95010d88fbc27df3d1c4e6838be7
Anton Schwaighofer <antonsc@microsoft.com>
2021-10-05 13:07:16 +0100

    manual optimization with single GPU loss function

--- InnerEye/ML/SSL/lightning_modules/simclr_module.py
+++ InnerEye/ML/SSL/lightning_modules/simclr_module.py
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-
+import math
 from typing import Any, Dict, List, Tuple, Union
 
 import torch
@@ -58,16 +58,18 @@ class SimCLRInnerEye(SimCLR):
         self.projection = _Projection(input_dim=self.encoder.get_output_feature_dim(), hidden_dim=2048, output_dim=128)
         # The optimizer for the linear head is managed by this module, so that its state can be stored in a checkpoint
         # automatically by Lightning. The training for the linear head is outside of this module though.
-        # self.automatic_optimization = False
+        self.automatic_optimization = False
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.encoder(x)
 
-    def training_step(self, batch: BatchType, batch_idx: int) -> None:  # type: ignore
+    def training_step(self, batch: BatchType, batch_idx: int, optimizer_idx: int) -> None:  # type: ignore
+        if optimizer_idx != 0:
+            return
         loss = self.shared_step(batch)
         log_on_epoch(self, "simclr/train/loss", loss)
         log_learning_rate(self, name="simclr/learning_rate")
-        return loss
+        manual_optimization_step(self, loss)
 
     def validation_step(self, batch: BatchType, batch_idx: int) -> T:  # type: ignore
         loss = self.shared_step(batch)
@@ -92,5 +94,38 @@ class SimCLRInnerEye(SimCLR):
 
     def configure_optimizers(self) -> Tuple[List[Optimizer], List[_LRScheduler]]:
         base_optim, base_scheduler = super().configure_optimizers()
-        # base_optim.append(self.online_eval_optimizer)
+        base_optim.append(self.online_eval_optimizer)
         return base_optim, base_scheduler
+
+
+    def nt_xent_loss(self, out_1, out_2, temperature, eps=1e-6):
+        """
+            assume out_1 and out_2 are normalized
+            out_1: [batch_size, dim]
+            out_2: [batch_size, dim]
+        """
+        out_1_dist = out_1
+        out_2_dist = out_2
+
+        # out: [2 * batch_size, dim]
+        # out_dist: [2 * batch_size * world_size, dim]
+        out = torch.cat([out_1, out_2], dim=0)
+        out_dist = torch.cat([out_1_dist, out_2_dist], dim=0)
+
+        # cov and sim: [2 * batch_size, 2 * batch_size * world_size]
+        # neg: [2 * batch_size]
+        cov = torch.mm(out, out_dist.t().contiguous())
+        sim = torch.exp(cov / temperature)
+        neg = sim.sum(dim=-1)
+
+        # from each row, subtract e^(1/temp) to remove similarity measure for x1.x1
+        row_sub = torch.Tensor(neg.shape).fill_(math.e**(1 / temperature)).to(neg.device)
+        neg = torch.clamp(neg - row_sub, min=eps)  # clamp for numerical stability
+
+        # Positive similarity, pos becomes [2 * batch_size]
+        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        pos = torch.cat([pos, pos], dim=0)
+
+        loss = -torch.log(pos / (neg + eps)).mean()
+
+        return loss
