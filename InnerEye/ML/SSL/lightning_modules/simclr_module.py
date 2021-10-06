8ae27460ff684e4aeab90ddc5af9ed3c12480fce
Anton Schwaighofer <antonsc@microsoft.com>
2021-10-06 15:36:32 +0100

    remove single GPU loss

--- InnerEye/ML/SSL/lightning_modules/simclr_module.py
+++ InnerEye/ML/SSL/lightning_modules/simclr_module.py
@@ -96,36 +96,3 @@ class SimCLRInnerEye(SimCLR):
         base_optim, base_scheduler = super().configure_optimizers()
         base_optim.append(self.online_eval_optimizer)
         return base_optim, base_scheduler
-
-
-    def nt_xent_loss(self, out_1, out_2, temperature, eps=1e-6):
-        """
-            assume out_1 and out_2 are normalized
-            out_1: [batch_size, dim]
-            out_2: [batch_size, dim]
-        """
-        out_1_dist = out_1
-        out_2_dist = out_2
-
-        # out: [2 * batch_size, dim]
-        # out_dist: [2 * batch_size * world_size, dim]
-        out = torch.cat([out_1, out_2], dim=0)
-        out_dist = torch.cat([out_1_dist, out_2_dist], dim=0)
-
-        # cov and sim: [2 * batch_size, 2 * batch_size * world_size]
-        # neg: [2 * batch_size]
-        cov = torch.mm(out, out_dist.t().contiguous())
-        sim = torch.exp(cov / temperature)
-        neg = sim.sum(dim=-1)
-
-        # from each row, subtract e^(1/temp) to remove similarity measure for x1.x1
-        row_sub = torch.Tensor(neg.shape).fill_(math.e**(1 / temperature)).to(neg.device)
-        neg = torch.clamp(neg - row_sub, min=eps)  # clamp for numerical stability
-
-        # Positive similarity, pos becomes [2 * batch_size]
-        pos = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
-        pos = torch.cat([pos, pos], dim=0)
-
-        loss = -torch.log(pos / (neg + eps)).mean()
-
-        return loss
