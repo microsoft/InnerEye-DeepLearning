2880485a93064db835b81f33ca8e251b164b821d
vsalvatelli <vsalvatelli@microsoft.com>
2021-10-08 14:11:27 +0000

    passing profiler from the container to the trainer

--- InnerEye/ML/model_training.py
+++ InnerEye/ML/model_training.py
@@ -15,19 +15,29 @@ from pytorch_lightning.plugins import DDPPlugin
 
 from InnerEye.Azure.azure_runner import ENV_GLOBAL_RANK, ENV_LOCAL_RANK, ENV_NODE_RANK
 from InnerEye.Azure.azure_util import RUN_CONTEXT, is_offline_run_context
-from InnerEye.Common.common_util import SUBJECT_METRICS_FILE_NAME, change_working_directory
+from InnerEye.Common.common_util import (
+    SUBJECT_METRICS_FILE_NAME,
+    change_working_directory,
+)
 from InnerEye.Common.resource_monitor import ResourceMonitor
-from InnerEye.ML.common import ModelExecutionMode, RECOVERY_CHECKPOINT_FILE_NAME, create_best_checkpoint
+from InnerEye.ML.common import (
+    ModelExecutionMode,
+    RECOVERY_CHECKPOINT_FILE_NAME,
+    create_best_checkpoint,
+)
 from InnerEye.ML.deep_learning_config import ARGS_TXT, VISUALIZATION_FOLDER
 from InnerEye.ML.lightning_base import InnerEyeContainer, InnerEyeLightning
 from InnerEye.ML.lightning_container import LightningContainer
 from InnerEye.ML.lightning_loggers import AzureMLLogger, StoringLogger
-from InnerEye.ML.lightning_models import SUBJECT_OUTPUT_PER_RANK_PREFIX, ScalarLightning, \
-    get_subject_output_file_per_rank
+from InnerEye.ML.lightning_models import (
+    SUBJECT_OUTPUT_PER_RANK_PREFIX,
+    ScalarLightning,
+    get_subject_output_file_per_rank,
+)
 
 TEMP_PREFIX = "temp/"
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 def is_global_rank_zero() -> bool:
@@ -86,23 +96,28 @@ class InnerEyeRecoveryCheckpointCallback(ModelCheckpoint):
     """
 
     def __init__(self, container: LightningContainer):
-        super().__init__(dirpath=str(container.checkpoint_folder),
-                         monitor="epoch",
-                         filename=RECOVERY_CHECKPOINT_FILE_NAME + "_{epoch}",
-                         period=container.recovery_checkpoint_save_interval,
-                         save_top_k=container.recovery_checkpoints_save_last_k,
-                         mode="max",
-                         save_last=False)
+        super().__init__(
+            dirpath=str(container.checkpoint_folder),
+            monitor="epoch",
+            filename=RECOVERY_CHECKPOINT_FILE_NAME + "_{epoch}",
+            period=container.recovery_checkpoint_save_interval,
+            save_top_k=container.recovery_checkpoints_save_last_k,
+            mode="max",
+            save_last=False,
+        )
 
-    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule, unused: bool = None) -> None:
+    def on_train_epoch_end(
+        self, trainer: Trainer, pl_module: LightningModule, unused: bool = None
+    ) -> None:
         pl_module.log(name="epoch", value=trainer.current_epoch)  # type: ignore
 
 
-def create_lightning_trainer(container: LightningContainer,
-                             resume_from_checkpoint: Optional[Path] = None,
-                             num_nodes: int = 1,
-                             **kwargs: Dict[str, Any]) -> \
-        Tuple[Trainer, Optional[StoringLogger]]:
+def create_lightning_trainer(
+    container: LightningContainer,
+    resume_from_checkpoint: Optional[Path] = None,
+    num_nodes: int = 1,
+    **kwargs: Dict[str, Any],
+) -> Tuple[Trainer, Optional[StoringLogger]]:
     """
     Creates a Pytorch Lightning Trainer object for the given model configuration. It creates checkpoint handlers
     and loggers. That includes a diagnostic logger for use in unit tests, that is also returned as the second
@@ -117,7 +132,9 @@ def create_lightning_trainer(container: LightningContainer,
     # models, this still appears to be the best way of choosing them because validation loss on the relatively small
     # training patches is not stable enough. Going by the validation loss somehow works for the Prostate model, but
     # not for the HeadAndNeck model.
-    last_checkpoint_callback = ModelCheckpoint(dirpath=str(container.checkpoint_folder), save_last=True, save_top_k=0)
+    last_checkpoint_callback = ModelCheckpoint(
+        dirpath=str(container.checkpoint_folder), save_last=True, save_top_k=0
+    )
 
     # Recovery checkpoints: {epoch} will turn into a string like "epoch=1"
     # Store 1 recovery checkpoint every recovery_checkpoint_save_interval epochs, keep the last
@@ -131,13 +148,20 @@ def create_lightning_trainer(container: LightningContainer,
         accelerator: Optional[str] = "ddp"
         # Initialize the DDP plugin. The default for pl_find_unused_parameters is False. If True, the plugin prints out
         # lengthy warnings about the performance impact of find_unused_parameters.
-        plugins = [DDPPlugin(num_nodes=num_nodes, sync_batchnorm=True,
-                             find_unused_parameters=container.pl_find_unused_parameters)]
+        plugins = [
+            DDPPlugin(
+                num_nodes=num_nodes,
+                sync_batchnorm=True,
+                find_unused_parameters=container.pl_find_unused_parameters,
+            )
+        ]
     else:
         accelerator = None
         plugins = []
     logging.info(f"Using {num_gpus} GPUs per node with accelerator '{accelerator}'")
-    tensorboard_logger = TensorBoardLogger(save_dir=str(container.logs_folder), name="Lightning", version="")
+    tensorboard_logger = TensorBoardLogger(
+        save_dir=str(container.logs_folder), name="Lightning", version=""
+    )
     loggers = [tensorboard_logger, AzureMLLogger()]
     storing_logger: Optional[StoringLogger]
     if isinstance(container, InnerEyeContainer):
@@ -167,27 +191,34 @@ def create_lightning_trainer(container: LightningContainer,
     if progress_bar_refresh_rate is None and is_azureml_run:
         # When running in AzureML, the default progress bar clutters the output files with thousands of lines.
         progress_bar_refresh_rate = 50
-        logging.info(f"The progress bar refresh rate is not set. Using a default of {progress_bar_refresh_rate}. "
-                     f"To change, modify the pl_progress_bar_refresh_rate field of the container.")
+        logging.info(
+            f"The progress bar refresh rate is not set. Using a default of {progress_bar_refresh_rate}. "
+            f"To change, modify the pl_progress_bar_refresh_rate field of the container."
+        )
     # Read out additional model-specific args here.
     # We probably want to keep essential ones like numgpu and logging.
-    trainer = Trainer(default_root_dir=str(container.outputs_folder),
-                      deterministic=deterministic,
-                      benchmark=benchmark,
-                      accelerator=accelerator,
-                      plugins=plugins,
-                      max_epochs=container.num_epochs,
-                      num_sanity_val_steps=container.pl_num_sanity_val_steps,
-                      callbacks=callbacks,
-                      logger=loggers,
-                      progress_bar_refresh_rate=progress_bar_refresh_rate,
-                      num_nodes=num_nodes,
-                      gpus=num_gpus,
-                      precision=precision,
-                      sync_batchnorm=True,
-                      terminate_on_nan=container.detect_anomaly,
-                      resume_from_checkpoint=str(resume_from_checkpoint) if resume_from_checkpoint else None,
-                      **kwargs)
+    trainer = Trainer(
+        default_root_dir=str(container.outputs_folder),
+        deterministic=deterministic,
+        benchmark=benchmark,
+        accelerator=accelerator,
+        plugins=plugins,
+        max_epochs=container.num_epochs,
+        profiler=container.profiler,
+        num_sanity_val_steps=container.pl_num_sanity_val_steps,
+        callbacks=callbacks,
+        logger=loggers,
+        progress_bar_refresh_rate=progress_bar_refresh_rate,
+        num_nodes=num_nodes,
+        gpus=num_gpus,
+        precision=precision,
+        sync_batchnorm=True,
+        terminate_on_nan=container.detect_anomaly,
+        resume_from_checkpoint=str(resume_from_checkpoint)
+        if resume_from_checkpoint
+        else None,
+        **kwargs,
+    )
     return trainer, storing_logger
 
 
@@ -198,18 +229,22 @@ def start_resource_monitor(config: LightningContainer) -> ResourceMonitor:
     # used for this folder might corrupt the file.
     gpu_csv = config.outputs_folder / "gpu_utilization"
     gpu_csv.mkdir(parents=True, exist_ok=True)
-    logging.info(f"Starting resource monitor. GPU utilization will be written to Tensorboard in "
-                 f"{gpu_tensorboard}, aggregate metrics to {gpu_csv}")
-    resource_monitor = ResourceMonitor(interval_seconds=config.monitoring_interval_seconds,
-                                       tensorboard_folder=gpu_tensorboard,
-                                       csv_results_folder=gpu_csv)
+    logging.info(
+        f"Starting resource monitor. GPU utilization will be written to Tensorboard in "
+        f"{gpu_tensorboard}, aggregate metrics to {gpu_csv}"
+    )
+    resource_monitor = ResourceMonitor(
+        interval_seconds=config.monitoring_interval_seconds,
+        tensorboard_folder=gpu_tensorboard,
+        csv_results_folder=gpu_csv,
+    )
     resource_monitor.start()
     return resource_monitor
 
 
-def model_train(checkpoint_path: Optional[Path],
-                container: LightningContainer,
-                num_nodes: int = 1) -> Tuple[Trainer, Optional[StoringLogger]]:
+def model_train(
+    checkpoint_path: Optional[Path], container: LightningContainer, num_nodes: int = 1
+) -> Tuple[Trainer, Optional[StoringLogger]]:
     """
     The main training loop. It creates the Pytorch model based on the configuration options passed in,
     creates a Pytorch Lightning trainer, and trains the model.
@@ -228,8 +263,10 @@ def model_train(checkpoint_path: Optional[Path],
     # Execute some bookkeeping tasks only once if running distributed:
     if is_global_rank_zero():
         logging.info(f"Model checkpoints are saved at {container.checkpoint_folder}")
-        write_args_file(container.config if isinstance(container, InnerEyeContainer) else container,
-                        outputs_folder=container.outputs_folder)
+        write_args_file(
+            container.config if isinstance(container, InnerEyeContainer) else container,
+            outputs_folder=container.outputs_folder,
+        )
         if container.monitoring_interval_seconds > 0:
             resource_monitor = start_resource_monitor(container)
 
@@ -249,17 +286,25 @@ def model_train(checkpoint_path: Optional[Path],
     # Set random seeds just before training. For segmentation models, we have
     # something that changes the random seed in the before_training_on_rank_zero hook.
     seed_everything(container.get_effective_random_seed())
-    trainer, storing_logger = create_lightning_trainer(container,
-                                                       checkpoint_path,
-                                                       num_nodes=num_nodes,
-                                                       **container.get_trainer_arguments())
-    rank_info = ", ".join(f"{env}: {os.getenv(env)}"
-                          for env in [ENV_GLOBAL_RANK, ENV_LOCAL_RANK, ENV_NODE_RANK])
-    logging.info(f"Environment variables: {rank_info}. trainer.global_rank: {trainer.global_rank}")
+    trainer, storing_logger = create_lightning_trainer(
+        container,
+        checkpoint_path,
+        num_nodes=num_nodes,
+        **container.get_trainer_arguments(),
+    )
+    rank_info = ", ".join(
+        f"{env}: {os.getenv(env)}"
+        for env in [ENV_GLOBAL_RANK, ENV_LOCAL_RANK, ENV_NODE_RANK]
+    )
+    logging.info(
+        f"Environment variables: {rank_info}. trainer.global_rank: {trainer.global_rank}"
+    )
     # InnerEye models use this logger for diagnostics
     if isinstance(lightning_model, InnerEyeLightning):
         if storing_logger is None:
-            raise ValueError("InnerEye models require the storing_logger for diagnostics")
+            raise ValueError(
+                "InnerEye models require the storing_logger for diagnostics"
+            )
         lightning_model.storing_logger = storing_logger
 
     logging.info("Starting training")
@@ -272,16 +317,26 @@ def model_train(checkpoint_path: Optional[Path],
     is_azureml_run = not is_offline_run_context(RUN_CONTEXT)
     # Per-subject model outputs for regression models are written per rank, and need to be aggregated here.
     # Each thread per rank will come here, and upload its files to the run outputs. Rank 0 will later download them.
-    if is_azureml_run and world_size > 1 and isinstance(lightning_model, ScalarLightning):
-        upload_output_file_as_temp(lightning_model.train_subject_outputs_logger.csv_path,  # type: ignore
-                                   container.outputs_folder)
-        upload_output_file_as_temp(lightning_model.val_subject_outputs_logger.csv_path,  # type: ignore
-                                   container.outputs_folder)
+    if (
+        is_azureml_run
+        and world_size > 1
+        and isinstance(lightning_model, ScalarLightning)
+    ):
+        upload_output_file_as_temp(
+            lightning_model.train_subject_outputs_logger.csv_path,  # type: ignore
+            container.outputs_folder,
+        )
+        upload_output_file_as_temp(
+            lightning_model.val_subject_outputs_logger.csv_path,  # type: ignore
+            container.outputs_folder,
+        )
     # DDP will start multiple instances of the runner, one for each GPU. Those should terminate here after training.
     # We can now use the global_rank of the Lightining model, rather than environment variables, because DDP has set
     # all necessary properties.
     if lightning_model.global_rank != 0:
-        logging.info(f"Terminating training thread with rank {lightning_model.global_rank}.")
+        logging.info(
+            f"Terminating training thread with rank {lightning_model.global_rank}."
+        )
         sys.exit()
 
     logging.info("Choosing the best checkpoint and removing redundant files.")
@@ -300,7 +355,10 @@ def model_train(checkpoint_path: Optional[Path],
             for rank in range(world_size):
                 for mode in [ModelExecutionMode.TRAIN, ModelExecutionMode.VAL]:
                     file = mode.value + "/" + get_subject_output_file_per_rank(rank)
-                    RUN_CONTEXT.download_file(name=TEMP_PREFIX + file, output_file_path=container.outputs_folder / file)
+                    RUN_CONTEXT.download_file(
+                        name=TEMP_PREFIX + file,
+                        output_file_path=container.outputs_folder / file,
+                    )
         # Concatenate all temporary file per execution mode
         aggregate_and_create_subject_metrics_file(container.outputs_folder)
 
@@ -308,13 +366,21 @@ def model_train(checkpoint_path: Optional[Path],
 
     # Upload visualization directory to AML run context to be able to see it in the Azure UI.
     if isinstance(container, InnerEyeContainer):
-        if container.config.max_batch_grad_cam > 0 and container.visualization_folder.exists():
-            RUN_CONTEXT.upload_folder(name=VISUALIZATION_FOLDER, path=str(container.visualization_folder))
+        if (
+            container.config.max_batch_grad_cam > 0
+            and container.visualization_folder.exists()
+        ):
+            RUN_CONTEXT.upload_folder(
+                name=VISUALIZATION_FOLDER, path=str(container.visualization_folder)
+            )
 
     if resource_monitor:
         logging.info("Shutting down the resource monitor process.")
         if is_azureml_run:
-            for gpu_name, metrics_per_gpu in resource_monitor.read_aggregate_metrics().items():
+            for (
+                gpu_name,
+                metrics_per_gpu,
+            ) in resource_monitor.read_aggregate_metrics().items():
                 # Log as a table, with GPU being the first column
                 RUN_CONTEXT.log_row("GPU utilization", GPU=gpu_name, **metrics_per_gpu)
         resource_monitor.kill()
@@ -330,7 +396,9 @@ def aggregate_and_create_subject_metrics_file(outputs_folder: Path) -> None:
     :param config: model config
     """
     for mode in [ModelExecutionMode.TRAIN, ModelExecutionMode.VAL]:
-        temp_files = sorted((outputs_folder / mode.value).rglob(SUBJECT_OUTPUT_PER_RANK_PREFIX + "*"))
+        temp_files = sorted(
+            (outputs_folder / mode.value).rglob(SUBJECT_OUTPUT_PER_RANK_PREFIX + "*")
+        )
         result_file = outputs_folder / mode.value / SUBJECT_METRICS_FILE_NAME
         with result_file.open("a") as f:
             for i, file in enumerate(temp_files):
