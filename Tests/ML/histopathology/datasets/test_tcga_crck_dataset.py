e1f1a2522ceb10194dcc84110de87472dc4f2d04
Melissa Bristow <mebristo@microsoft.com>
2021-12-09 12:23:23 +0000

    Add datasets tests and util

---
+++ Tests/ML/histopathology/datasets/test_tcga_crck_dataset.py
@@ -0,0 +1,58 @@
+import os
+
+import pandas as pd
+import pytest
+import torch
+from monai.data.dataset import Dataset
+from torch.utils.data import DataLoader
+
+from InnerEye.ML.Histopathology.datasets.default_paths import TCGA_CRCK_DATASET_DIR
+from InnerEye.ML.Histopathology.datasets.tcga_crck_tiles_dataset import TcgaCrck_TilesDataset
+from InnerEye.ML.Histopathology.models.transforms import LoadTiled
+from InnerEye.ML.Histopathology.utils.tcga_utils import extract_fields
+
+
+def test_extract_fields() -> None:
+    slide_id = "TCGA-XX-0123"
+    tile_id = "ABCDEFGHIJKL"
+    split = "train"
+    label = 0
+    path = (f"CRC_DX_{split.upper()}/"
+            f"{['MSS', 'MSIMUT'][label]}/"
+            f"blk-{tile_id}-{slide_id}-01Z-00-DX1.png")
+    fields = {
+        'slide_id': slide_id,
+        'tile_id': tile_id,
+        'image': path,
+        'split': split,
+        'label': label
+    }
+    extracted_fields = extract_fields(pd.Series(fields))
+    assert fields == extracted_fields
+
+
+@pytest.mark.skipif(not os.path.isdir(TCGA_CRCK_DATASET_DIR),
+                    reason="TCGA-CRCk dataset is unavailable")
+@pytest.mark.parametrize('train', [True, False])
+def test_dataset(train: bool) -> None:
+    base_dataset = TcgaCrck_TilesDataset(TCGA_CRCK_DATASET_DIR, train=train)
+    dataset = Dataset(base_dataset, transform=LoadTiled('image'))  # type: ignore
+
+    expected_length = 93408 if train else 98904
+    assert len(dataset) == expected_length
+
+    sample = dataset[0]
+    expected_keys = ['slide_id', 'tile_id', 'image', 'split', 'label']
+    assert all(key in sample for key in expected_keys)
+    assert isinstance(sample['image'], torch.Tensor)
+    assert sample['image'].shape == (3, 224, 224)
+
+    batch_size = 16
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)  # type: ignore
+    batch = next(iter(loader))
+    assert all(key in batch for key in expected_keys)
+    assert isinstance(batch['image'], torch.Tensor)
+    assert batch['image'].shape == (batch_size, 3, 224, 224)
+    assert batch['image'].dtype == torch.float32
+    assert batch['label'].shape == (batch_size,)
+    assert batch['label'].dtype == torch.int64
