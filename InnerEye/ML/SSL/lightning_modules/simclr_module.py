888f03727a8a47427669fd3ce5d17bf9255422e5
Javier Alvarez-Valle <jaalvare@microsoft.com>
2022-01-14 12:53:03 +0000

    Add ORT

--- InnerEye/ML/SSL/lightning_modules/simclr_module.py
+++ InnerEye/ML/SSL/lightning_modules/simclr_module.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from health_ml.utils import log_learning_rate, log_on_epoch
 from pl_bolts.models.self_supervised.simclr.simclr_module import SimCLR
-
+import torch_ort
 from InnerEye.ML.SSL.encoders import SSLEncoder
 from InnerEye.ML.SSL.utils import SSLDataModuleType
 
@@ -51,8 +51,8 @@ class SimCLRInnerEye(SimCLR):
             kwargs.update({"dataset": dataset_name})
         super().__init__(**kwargs)
         self.save_hyperparameters()
-        self.encoder = SSLEncoder(encoder_name, use_7x7_first_conv_in_resnet)
-        self.projection = _Projection(input_dim=self.encoder.get_output_feature_dim(), hidden_dim=2048, output_dim=128)
+        self.encoder =  torch_ort.ORTModule(SSLEncoder(encoder_name, use_7x7_first_conv_in_resnet))
+        self.projection = torch_ort.ORTModule(_Projection(input_dim=self.encoder.get_output_feature_dim(), hidden_dim=2048, output_dim=128))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.encoder(x)
