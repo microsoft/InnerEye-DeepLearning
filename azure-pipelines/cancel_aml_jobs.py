ff6ec6c0aca61dbd05b90a47c52cbc9990580362
Anton Schwaighofer <antonsc@microsoft.com>
2022-01-24 15:26:17 +0000

    cleanup again

--- azure-pipelines/cancel_aml_jobs.py
+++ azure-pipelines/cancel_aml_jobs.py
@@ -33,7 +33,8 @@ def cancel_running_and_queued_jobs() -> None:
     print(f"Retrieved experiment {experiment.name}")
     for run in experiment.get_runs(include_children=True, properties={}):
         assert isinstance(run, Run)
-        do_cancel = run.status not in (RunStatus.COMPLETED, RunStatus.FAILED, RunStatus.FINALIZING)
+        do_cancel = run.status not in (RunStatus.COMPLETED, RunStatus.FAILED, RunStatus.FINALIZING, RunStatus.CANCELED,
+                                       RunStatus.CANCEL_REQUESTED)
         operation = "Cancelling" if do_cancel else "Skipping"
         print(f"{operation} '{run.status}' run {run.id}: ({run.display_name})")
 
