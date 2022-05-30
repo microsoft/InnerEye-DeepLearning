592eef9de8288b75bc3db609db8a13167a90dbb6
Peter Hessey <a-phessey@microsoft.com>
2022-05-30 12:29:36 +0100

    📝 ✨ Finish script, add documentation

--- create_and_lock_environment.sh
+++ create_and_lock_environment.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-# create conda environment
-export CONDA_ALWAYS_YES="true"
-conda env create --file primary_deps.yml
+os_name=$(uname)
+if [[ ! $os_name == *"Linux"* ]]; then
+    echo "ERROR: cannot run AML environment locking in non-linux environment. Windows users can do this using WSL - https://docs.microsoft.com/en-us/windows/wsl/install"
+    exit 1
+else
+    echo "Starting AML environment locking..."
+fi
+
+# get environment name from primary dependencies YAML file
 name_line="$(cat primary_deps.yml | grep 'name:')"
 IFS=':' read -ra name_arr <<< "$name_line"
 env_name="${name_arr[1]}"
+
+# clear old conda envs, create new one
+export CONDA_ALWAYS_YES="true"
+conda env remove --name ${env_name::-1}
+conda env create --file primary_deps.yml
+
+# export new environment to environment.yml
 conda env export -n ${env_name::-1} | grep -v "prefix:" > environment.yml
 unset CONDA_ALWAYS_YES
 
-# remove python version hash
+# remove python version hash (technically not locked, so still potential for problems here if python secondary deps change)
 while IFS='' read -r line; do
     if [[ $line == *"- python="* ]]; then
 
@@ -21,6 +34,3 @@ while IFS='' read -r line; do
     fi
 done < environment.yml > environment.yml.tmp
 mv environment.yml.tmp environment.yml
-
-echo Activate your environment by running the following command:
-echo "      conda activate" ${env_name}
