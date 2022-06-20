ed8f6df82564aeaeb5004c10e1a2aff98668a3f2
Anna Schroder <t-aschroder@microsoft.com>
2022-06-20 13:34:31 +0000

    fixes to uncertainty_aggregate.py

--- InnerEye/ML/uncertainty_aggregate.py
+++ InnerEye/ML/uncertainty_aggregate.py
@@ -15,18 +15,13 @@ import SimpleITK as sitk
 from InnerEye.Common.common_util import BEST_EPOCH_FOLDER_NAME, METRICS_AGGREGATES_FILE, ModelProcessing, \
     SUBJECT_METRICS_FILE_NAME, get_best_epoch_results_path, is_linux, logging_section
 from InnerEye.ML.common import ModelExecutionMode
+import pickle
 
 # think about what the inputs will be:
 # output trained random forest model 
 
 
 # possibility to add extra functionality to simulate poor segmentations 
-'''
-class UncertaintyAggregate():
-
-    def __init__(self,config):
-        self.outputs_folder = config.outputs_folder
-'''
 
 def trainRandomForest(x,y):
     # requires dataset: subjects, groups
@@ -45,16 +40,16 @@ def splitKfolds():
     return k_folds.split(subject_ids, groups=groups)
 '''
 
-def UncertaintyMapFeatures(self,subjects):
+def UncertaintyMapFeatures(dice,outputs_folder,subjects):
     '''extract PyRadiomics features from the uncertainty map'''
-
+    features = pd.DataFrame()
     for i,subject in enumerate(subjects):
-        subject_dir = self.outputs_folder / f"{int(patient_id):03d}"
+        subject_dir = outputs_folder / f"{int(patient_id):03d}"
         extractor = featureextractor.RadiomicsFeatureExtractor(imageType='Original',binWidth=25)
         result=pd.Series(extractor.execute(
             str(subject_dir / 'uncertainty.nii.gz'), 
             str(subject_dir / 'segmentation.nii.gz')))
-        featureVector=self.df[subj_i]
+        featureVector=df[subj_i]
         featureVector=featureVector.append(result)
         featureVector.name=subj
 
@@ -64,6 +59,9 @@ def UncertaintyMapFeatures(self,subjects):
     return features
 
 def PostProcessFeatures(features):
+    '''drop unused pyRadiomics features
+    TO DO: pyradiomics website, create .yml file which selects the desired features'''
+
     features = features.T
 
     features=features.drop(['diagnostics_Versions_PyRadiomics',
@@ -87,6 +85,7 @@ def PostProcessFeatures(features):
 
     # shuffle rows - currently ordered by accending Dice score
     #features = shuffle(features,random_state=0)
+    return features
 
 
 def load_uncertainty_from_nifti(fname):
@@ -95,8 +94,8 @@ def load_uncertainty_from_nifti(fname):
 
 def average_dice_across_structures(subjects,metrics):
     avgDice=[]
-    for subjedct_id in subjects:
-        avgDice.append(np.mean(metrics.Dice[metrics.Patient==subj]))
+    for subject_id in subjects:
+        avgDice.append(np.mean(metrics.Dice[metrics.Patient==subject_id]))
     return avgDice
 
 def UncertaintyAggregate(config):
@@ -106,21 +105,18 @@ def UncertaintyAggregate(config):
     metrics_df = pd.read_csv(outputs_folder / 'best_validation_epoch/Test/metrics.csv')
     subjects = metrics_df.Patient.unique()
 
-    features = pd.DataFrame()
-    df.dice = average_dice_across_structures(self,subjects,metrics_df)
-    features = UncertaintyMapFeatures(self, features, subject_id, i)
-        #dice.append(average_dice_across_structures())
-
-    features = self.PostProcessFeatures(features)
+    # why are two dataframes needed???
+    dice = pd.DataFrame(average_dice_across_structures(subjects,metrics_df))
+    features = UncertaintyMapFeatures(dice,outputs_folder,subjects)
+    features = PostProcessFeatures(features)
     # cross-validation to estimate model performance
     
     # train model with all data
-    mdl = trainRandomForest(features,dice)
+    mdl = trainRandomForest(features.drop(['dice']),features.dice)
 
     # save model to disk - save in outputs file? Where will it be seen by the inference model?
     fname = 'uncertainty_metric_model'
     pickle.dump(mdl, open(fname, 'wb'))
 
     # could run cross-validation and save some metrics in the outputs file to report how well the model is about the predict dice
-
     return 
