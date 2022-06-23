38f830374e705eda2cb38963e51ab1293860c1e8
Peter Hessey <45258028+peterhessey@users.noreply.github.com>
2022-06-23 09:44:54 +0100

    ENH: Move hi-ml and fastMRI to PyPI packages (#752)
    * 🔥 💥 Remove submodules
    
    * ➕ Add packages to primary dependencies
    
    * 📌 ⬆️ Upgrade python to 3.8 and lock
    
    * ✅ Fix fastMRI test
    
    * 💚 ✅ Fix tests and CI workflows
    
    * 🏷️ Fix MyPy and Flake8 issues
    
    * ⬆️ Update windows dependencies
    
    * 📝 Add fastMRI reference
    
    * ⚰️ Remove references to old submodules

--- InnerEye/ML/run_ml.py
+++ InnerEye/ML/run_ml.py
@@ -654,11 +654,7 @@ class MLRunner:
         # we can identify it by going up the folder structure off a known file (repository_root does exactly that)
         repository_root = fixed_paths.repository_root_directory()
         copy_folder(repository_root / INNEREYE_PACKAGE_NAME)
-        # If hi-ml is used as a submodule, copy that too
-        for relative_path, _ in fixed_paths.get_hi_ml_submodule_relative_paths():
-            full_submodule_path = repository_root / relative_path
-            if full_submodule_path.is_dir():
-                copy_folder(full_submodule_path, relative_path)
+
         # Extra code directory is expected to be relative to the project root folder.
         if self.azure_config.extra_code_directory:
             extra_code_folder = self.project_root / self.azure_config.extra_code_directory
