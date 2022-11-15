48647f972a1b312516f4e37676f0937702a3c863
Peter Hessey <a-phessey@microsoft.com>
2022-11-15 16:03:33 +0000

    ✅ Update regression test data

--- InnerEye/ML/configs/regression/LungRegression.py
+++ InnerEye/ML/configs/regression/LungRegression.py
@@ -8,18 +8,16 @@ class LungRegression(Lung):
     def __init__(self) -> None:
         super().__init__(
             azure_dataset_id="lung",
-            train_batch_size=2,
-            test_crop_size=(32, 144, 144),
-            crop_size=(32, 144, 144),
-            inference_stride_size=(32, 144, 144),
-            num_epochs=150,
+            train_batch_size=3,
+            num_epochs=25,
             pl_deterministic=True,
+            test_crop_size=(64, 256, 256),
         )
 
     def get_model_train_test_dataset_splits(self, dataset_df: pd.DataFrame) -> DatasetSplits:
         train = list(map(str, range(0, 10)))
-        val = list(map(str, range(10, 15)))
-        test = list(map(str, range(15, 16)))
+        val = list(map(str, range(10, 12)))
+        test = list(map(str, range(12, 13)))
 
         return DatasetSplits.from_subject_ids(
             df=dataset_df,
